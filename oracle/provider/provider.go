625ee5529b9b9c2b3aeb25caa1fd1c67dda2412e
Aleksandr Bezobchuk <aleks.bezobchuk@gmail.com>
2023-06-05 12:38:20 -0400

    updates

---
+++ oracle/provider/provider.go
@@ -0,0 +1,46 @@
+package provider
+
+import (
+	"github.com/skip-mev/slinky/oracle/types"
+)
+
+type (
+	// Provider defines an interface an exchange price provider must implement.
+	Provider interface {
+		// Name returns the name of the provider.
+		Name() string
+
+		// GetTickerPrices returns the tickerPrices based on the provided pairs.
+		GetTickerPrices(...types.CurrencyPair) (map[string]types.TickerPrice, error)
+
+		// GetCandlePrices returns the candlePrices based on the provided pairs.
+		GetCandlePrices(...types.CurrencyPair) (map[string][]types.CandlePrice, error)
+
+		// GetAvailablePairs return all available pairs symbol to subscribe.
+		GetAvailablePairs() (map[string]struct{}, error)
+	}
+
+	// AggregatedProviderPrices defines a type alias for a map
+	// of provider -> asset -> TickerPrice
+	AggregatedProviderPrices map[string]map[string]types.TickerPrice
+
+	// AggregatedProviderCandles defines a type alias for a map
+	// of provider -> asset -> []types.CandlePrice
+	AggregatedProviderCandles map[string]map[string][]types.CandlePrice
+
+	// Endpoint defines an override setting in our config for the
+	// hardcoded rest and websocket api endpoints.
+	Endpoint struct {
+		// Name of the provider, ex. "binance"
+		Name string `toml:"name"`
+
+		// Rest endpoint for the provider, ex. "https://api1.binance.com"
+		Rest string `toml:"rest"`
+
+		// Websocket endpoint for the provider, ex. "stream.binance.com:9443"
+		Websocket string `toml:"websocket"`
+
+		// APIKey for API Key protected endpoints
+		APIKey string `toml:"apikey"`
+	}
+)
