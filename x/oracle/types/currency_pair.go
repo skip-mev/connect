8636c765562513e96c6447e89a9713bb6766bb99
Nikhil Vasan <nikhil@skip.money>
2023-06-14 15:09:16 -0700

    keeper methods + tests

---
+++ x/oracle/types/currency_pair.go
@@ -0,0 +1,33 @@
+package types
+
+import (
+	"strings"
+	"fmt"
+)
+
+// ValidateBasic checks that the Base / Quote strings in the CurrencyPair are formatted correctly
+func (cp CurrencyPair) ValidateBasic() error {
+	// check formatting of base / quote
+	if strings.ToUpper(cp.Base) != cp.Base {
+		return fmt.Errorf("incorrectly formatted base string, expected: %s got: %s", strings.ToUpper(cp.Base), strings.ToUpper(cp.Base))
+	}
+	if strings.ToUpper(cp.Quote) != cp.Quote {
+		return fmt.Errorf("incorrectly formatted quote string, expected: %s got: %s", strings.ToUpper(cp.Quote), strings.ToUpper(cp.Quote))
+	}
+	return nil
+}
+
+func (cp CurrencyPair) ToString() string {
+	return fmt.Sprintf("%s/%s", cp.Base, cp.Quote)
+}
+
+func CurrencyPairFromString(s string) CurrencyPair {
+	split := strings.Split(s, "/")
+	if len(split) != 2 {
+		return CurrencyPair{}
+	}
+	return CurrencyPair{
+		Base: split[0],
+		Quote: split[1],
+	}
+}
---
+++ x/oracle/types/currency_pair_test.go
@@ -0,0 +1,81 @@
+package types_test
+
+import (
+	"testing"
+
+	"github.com/skip-mev/slinky/x/oracle/types"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestValidateBasic(t *testing.T) {
+	tcs := []struct{
+		name string
+		cp types.CurrencyPair
+		expectPass bool
+	}{
+		{
+			"if the Base is not upper-case - fail",
+			types.CurrencyPair{
+				Base: "aB",
+				Quote: "BB",
+			},
+			false,
+		},
+		{
+			"if the Quote is not upper-case - fail",
+			types.CurrencyPair{
+				Base: "BB",
+				Quote: "aB",
+			},
+			false,
+		},
+		{
+			"if both Quote + Base are formatted correctly - pass",
+			types.CurrencyPair{
+				Base: "BB",
+				Quote: "AA",
+			},
+			true,
+		},
+	}
+
+	for _, tc := range tcs {
+		t.Run(tc.name, func(t *testing.T) {
+			err := tc.cp.ValidateBasic()
+			switch tc.expectPass {
+			case true:
+				assert.Nil(t, err)
+			default:
+				assert.NotNil(t, err)
+			}
+
+		})
+	}
+}
+
+func TestToFromString(t *testing.T) {
+	tcs := []struct{
+		name string
+		// string formatted CurrencyPair
+		cps string
+		cp types.CurrencyPair
+	}{
+		{
+			"if string is incorrectly formatted, return an empty CurrencyPair",
+			"aa",
+			types.CurrencyPair{},
+		},
+		{
+			"if the string is correctly formatted, return the original CurrencyPair",
+			types.CurrencyPair{Base: "A", Quote: "B"}.ToString(),
+			types.CurrencyPair{Base: "A", Quote: "B"},
+		},
+	}
+
+	for _, tc := range tcs {
+		t.Run(tc.name, func(t *testing.T) {
+			cp := types.CurrencyPairFromString(tc.cps)
+			assert.Equal(t, cp, tc.cp)
+		})
+	}
+}
