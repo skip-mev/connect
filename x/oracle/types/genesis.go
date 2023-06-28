f6a1c4ab80d1fc6d7f8821eba6d3ffaa65f12c84
Nikhil Vasan <97126437+nivasan1@users.noreply.github.com>
2023-06-28 18:07:51 -0400

    feat(module): read genesis from app-state [ENG-1293] (#47)

--- x/oracle/types/genesis.go
+++ x/oracle/types/genesis.go
@@ -1,6 +1,11 @@
 package types
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+
+	"github.com/cosmos/cosmos-sdk/codec"
+)
 
 // ValidateBasic validates that the CurrencyPair is valid, and performs any necessary validation on the
 // genesis QuotePrice for the CurrencyPair. This fails if the CurrencyPair is invalid, or if the QuotePrice is nil,
@@ -39,4 +44,16 @@ func (gs GenesisState) Validate() error {
 		}
 	}
 	return nil
+}
+
+// GetGenesisStateFromAppState returns x/oracle GenesisState given raw application
+// genesis state.
+func GetGenesisStateFromAppState(cdc codec.Codec, appState map[string]json.RawMessage) GenesisState {
+	var genesisState GenesisState
+
+	if appState[ModuleName] != nil {
+		cdc.MustUnmarshalJSON(appState[ModuleName], &genesisState)
+	}
+
+	return genesisState
 }
