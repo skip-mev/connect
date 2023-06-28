f2e923e3a5e0ecd33dcaedfe3be88ed9fc94f009
David Terpay <35130517+davidterpay@users.noreply.github.com>
2023-06-28 19:18:46 -0400

    feat(e2e): Setting up docker containers running binaries (#45)

---
+++ tests/e2e/e2e_test.go
@@ -0,0 +1,8 @@
+package e2e
+
+// TestSetUp tests that the network is spun up correctly.
+//
+// TODO: remove this once we write real test cases.
+func (s *IntegrationTestSuite) TestSetUp() {
+	s.Require().True(true)
+}
