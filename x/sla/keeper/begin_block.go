8bc6f023ac0f958a4e641c73843cf7dfd7ffcb96
David Terpay <35130517+davidterpay@users.noreply.github.com>
2023-11-09 10:50:22 -0500

    feat(SLA): Module configuration for x/sla (#198)
    Co-authored-by: Alex Johnson <alex@djinntek.world>

---
+++ x/sla/keeper/begin_block.go
@@ -0,0 +1,33 @@
+package keeper
+
+import (
+	cometabci "github.com/cometbft/cometbft/abci/types"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+)
+
+// BeginBlocker is called at the start of every block. This will fetch
+// all of the SLAs from state and execute them against the current set
+// of price feeds the network is maintaining.
+func (k Keeper) BeginBlocker(ctx sdk.Context) ([]cometabci.ValidatorUpdate, error) {
+	params, err := k.GetParams(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	if !params.Enabled {
+		return nil, nil
+	}
+
+	slas, err := k.GetSLAs(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	for _, sla := range slas {
+		if err := k.ExecSLA(ctx, sla); err != nil {
+			return nil, err
+		}
+	}
+
+	return nil, nil
+}
