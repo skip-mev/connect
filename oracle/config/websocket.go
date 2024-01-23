5e9d825b6561979bee784c809190805e11144bbb
David Terpay <david.terpay@gmail.com>
2024-01-23 16:56:42 -0500

    init

--- oracle/config/websocket.go
+++ oracle/config/websocket.go
@@ -24,6 +24,34 @@ type WebSocketConfig struct {
 
 	// Name is the name of the provider that corresponds to this config.
 	Name string `mapstructure:"name" toml:"name"`
+
+	// ReadBufferSize specifies the I/O read buffer size. if a buffer size of 0 is
+	// specified, then a default buffer size is used.
+	ReadBufferSize int `mapstructure:"read_buffer_size" toml:"read_buffer_size"`
+
+	// WriteBufferSize specifies the I/O write buffer size. if a buffer size of 0 is
+	// specified, then a default buffer size is used.
+	WriteBufferSize int `mapstructure:"write_buffer_size" toml:"write_buffer_size"`
+
+	// HandshakeTimeout specifies the duration for the handshake to complete.
+	HandshakeTimeout time.Duration `mapstructure:"handshake_timeout" toml:"handshake_timeout"`
+
+	// EnableCompression specifies if the client should attempt to negotiate per
+	// message compression (RFC 7692). Setting this value to true does not guarantee
+	// that compression will be supported.
+	EnableCompression bool `mapstructure:"enable_compression" toml:"enable_compression"`
+
+	// ReadDeadline sets the read deadline on the underlying network connection.
+	// After a read has timed out, the websocket connection state is corrupt and
+	// all future reads will return an error. A zero value for t means reads will
+	// not time out.
+	ReadDeadline time.Duration `mapstructure:"read_deadline" toml:"read_deadline"`
+
+	// WriteDeadline sets the write deadline on the underlying network
+	// connection. After a write has timed out, the websocket state is corrupt and
+	// all future writes will return an error. A zero value for t means writes will
+	// not time out.
+	WriteDeadline time.Duration `mapstructure:"write_deadline" toml:"write_deadline"`
 }
 
 // ValidateBasic performs basic validation of the websocket config.
