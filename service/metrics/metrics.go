eae6db11b59e99358746b3bd306d0dd1a03e5c18
Nikhil Vasan <nikhil@skip.money>
2024-01-23 13:54:25 -0800

    add metrics for pre-block latency

--- service/metrics/metrics.go
+++ service/metrics/metrics.go
@@ -51,7 +51,7 @@ const (
 	ProcessProposal
 	ExtendVote
 	VerifyVoteExtension
-	FinalizeBlock
+	PreBlock
 )
 
 func (a ABCIMethod) String() string {
@@ -64,7 +64,7 @@ func (a ABCIMethod) String() string {
 		return "extend_vote"
 	case VerifyVoteExtension:
 		return "verify_vote_extension"
-	case FinalizeBlock:
+	case PreBlock:
 		return "finalize_block"
 	default:
 		return "not_implemented"
