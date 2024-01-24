ac9d652d8b68ad84def300bba0687cdd1e114b84
Nikhil Vasan <97126437+nivasan1@users.noreply.github.com>
2024-01-24 15:21:21 -0500

    fix: oracle client + servers [BLO-804] (#36)
    * add blocking dial option for GRPC clients
    
    * update server to close GRPC server on shutdown
    
    * lint
    
    * docs

--- tests/simapp/app.go
+++ tests/simapp/app.go
@@ -1,6 +1,7 @@
 package simapp
 
 import (
+	"context"
 	"io"
 	"os"
 	"path/filepath"
@@ -317,9 +318,11 @@ func NewSimApp(
 		go app.oraclePrometheusServer.Start()
 	}
 
-	// Connect to the oracle service.
+	// Connect to the oracle service (default timeout of 5 seconds).
+	dialCtx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
 	go func() {
-		if err := app.oracleClient.Start(); err != nil {
+		if err := app.oracleClient.Start(dialCtx); err != nil {
 			app.Logger().Error("failed to start oracle client", "err", err)
 			panic(err)
 		}
