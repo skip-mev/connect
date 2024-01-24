2d4504d168467a03085cbea498aae0e876fe9aac
aljo242 <alex@skip.money>
2024-01-24 17:08:06 -0500

    fix

--- config/local/generate_toml.go
+++ config/local/generate_toml.go
@@ -15,6 +15,7 @@ import (
 	"github.com/skip-mev/slinky/providers/apis/binance"
 	"github.com/skip-mev/slinky/providers/apis/coinbase"
 	"github.com/skip-mev/slinky/providers/apis/coingecko"
+	"github.com/skip-mev/slinky/providers/websockets/bybit"
 	"github.com/skip-mev/slinky/providers/websockets/cryptodotcom"
 	"github.com/skip-mev/slinky/providers/websockets/kraken"
 	"github.com/skip-mev/slinky/providers/websockets/okx"
@@ -364,6 +365,55 @@ var LocalConfig = config.OracleConfig{
 					},
 					"ETHEREUM/BITCOIN": {
 						Ticker:       "ETH/XBT",
+						CurrencyPair: oracletypes.NewCurrencyPair("ETHEREUM", "BITCOIN"),
+					},
+				},
+			},
+		},
+		{
+			Name: bybit.Name,
+			WebSocket: config.WebSocketConfig{
+				Name:                bybit.Name,
+				Enabled:             true,
+				MaxBufferSize:       1000,
+				ReconnectionTimeout: 10 * time.Second,
+				WSS:                 bybit.ProductionURL,
+				ReadBufferSize:      config.DefaultReadBufferSize,
+				WriteBufferSize:     config.DefaultWriteBufferSize,
+				HandshakeTimeout:    config.DefaultHandshakeTimeout,
+				EnableCompression:   config.DefaultEnableCompression,
+				ReadTimeout:         config.DefaultReadTimeout,
+				WriteTimeout:        config.DefaultWriteTimeout,
+			},
+			Market: config.MarketConfig{
+				Name: bybit.Name,
+				CurrencyPairToMarketConfigs: map[string]config.CurrencyPairMarketConfig{
+					"BITCOIN/USD": {
+						Ticker:       "BTCUSDT",
+						CurrencyPair: oracletypes.NewCurrencyPair("BITCOIN", "USD"),
+					},
+					"ETHEREUM/USD": {
+						Ticker:       "ETHUSDT",
+						CurrencyPair: oracletypes.NewCurrencyPair("ETHEREUM", "USD"),
+					},
+					"ATOM/USD": {
+						Ticker:       "ATOMUSDT",
+						CurrencyPair: oracletypes.NewCurrencyPair("ATOM", "USD"),
+					},
+					"SOLANA/USD": {
+						Ticker:       "SOLUSDT",
+						CurrencyPair: oracletypes.NewCurrencyPair("SOLANA", "USD"),
+					},
+					"AVAX/USD": {
+						Ticker:       "AVAXUSDT",
+						CurrencyPair: oracletypes.NewCurrencyPair("AVAX", "USD"),
+					},
+					"DYDX/USD": {
+						Ticker:       "DYDXUSDT",
+						CurrencyPair: oracletypes.NewCurrencyPair("DYDX", "USD"),
+					},
+					"ETHEREUM/BITCOIN": {
+						Ticker:       "ETHBTC",
 						CurrencyPair: oracletypes.NewCurrencyPair("ETHEREUM", "BITCOIN"),
 					},
 				},
