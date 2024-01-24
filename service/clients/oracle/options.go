905bef45ee9fd1e50d07dc7f78db269fe1ee3853
Nikhil Vasan <nikhil@skip.money>
2024-01-24 12:20:52 -0800

    docs

--- service/clients/oracle/options.go
+++ service/clients/oracle/options.go
@@ -4,6 +4,8 @@ package oracle
 type Option func(OracleClient)
 
 // WithBlockingDial configures the OracleClient to block on dialing the remote oracle server.
+//
+// NOTICE: This option is not recommended to be used in practice. See the [GRPC docs](https://github.com/grpc/grpc-go/blob/master/Documentation/anti-patterns.md)
 func WithBlockingDial() Option {
 	return func(c OracleClient) {
 		client, ok := c.(*GRPCClient)
