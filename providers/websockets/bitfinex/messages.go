dfe83341ddea93d2ae13b2905ba2270d0f94cd5f
aljo242 <alex@skip.money>
2024-01-25 13:52:13 -0500

    wip

--- providers/websockets/bitfinex/messages.go
+++ providers/websockets/bitfinex/messages.go
@@ -1,1 +1,33 @@
 package bitfinex
+
+type (
+	// Event is the event type of a message sent over the Bitfinex websocket API.
+	Event string
+
+	// Channel is the channel of a message sent over the Bitfinex websocket API.
+	Channel string
+)
+
+const (
+	EVENT_SUBSCRIBE  Event = "subscribe"
+	EVENT_SUBSCRIBED Event = "subscribed"
+
+	CHANNEL_TICKER Channel = "ticker"
+)
+
+type BaseMessage struct {
+	Event   string `json:"event" validate:"required"`
+	Channel string `json:"channel" validate:"required"`
+	Symbol  string `json:"symbol" validate:"required"`
+}
+
+type SubscribeMessage BaseMessage
+
+type SubscribedMessage struct {
+	BaseMessage
+	ChannelID string `json:"chanId" validate:"required"`
+	Pair      string `json:"pair" validate:"required"`
+}
+
+type TickerStream struct {
+}
