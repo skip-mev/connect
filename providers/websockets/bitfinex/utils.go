ed1bd7ceac2d0c509e46a5bf14bb256d872567d1
aljo242 <alex@skip.money>
2024-01-25 14:32:09 -0500

    util

--- providers/websockets/bitfinex/utils.go
+++ providers/websockets/bitfinex/utils.go
@@ -1,1 +1,26 @@
 package bitfinex
+
+import "github.com/skip-mev/slinky/oracle/config"
+
+const (
+	// Name is the name of the BitFinex provider.
+	Name = "bitfinex"
+
+	// URLProd is the public BitFinex Websocket URL.
+	URLProd = "wss://api-pub.bitfinex.com/ws/2"
+)
+
+// DefaultWebSocketConfig is the default configuration for the BitFinex Websocket.
+var DefaultWebSocketConfig = config.WebSocketConfig{
+	Name:                Name,
+	Enabled:             true,
+	MaxBufferSize:       1000,
+	ReconnectionTimeout: config.DefaultReconnectionTimeout,
+	WSS:                 URLProd,
+	ReadBufferSize:      config.DefaultReadBufferSize,
+	WriteBufferSize:     config.DefaultWriteBufferSize,
+	HandshakeTimeout:    config.DefaultHandshakeTimeout,
+	EnableCompression:   config.DefaultEnableCompression,
+	ReadTimeout:         config.DefaultReadTimeout,
+	WriteTimeout:        config.DefaultWriteTimeout,
+}
