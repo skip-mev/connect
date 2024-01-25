e8ab41dbe2b6bb6a05ee63a215689c1d7ac91721
David Terpay <david.terpay@gmail.com>
2024-01-24 19:39:30 -0500

    bing bong

--- providers/base/websocket/handlers/ws_conn_handler.go
+++ providers/base/websocket/handlers/ws_conn_handler.go
@@ -90,6 +90,10 @@ func (h *WebSocketConnHandlerImpl) Read() ([]byte, error) {
 		return nil, fmt.Errorf("connection has not been established")
 	}
 
+	if h.conn == nil {
+		return nil, fmt.Errorf("connection has not been established")
+	}
+
 	// Set the read deadline to the configured read timeout.
 	if err := h.conn.SetReadDeadline(time.Now().Add(h.cfg.ReadTimeout)); err != nil {
 		return nil, err
@@ -109,6 +113,10 @@ func (h *WebSocketConnHandlerImpl) Write(message []byte) error {
 		return fmt.Errorf("connection has not been established")
 	}
 
+	if h.conn == nil {
+		return fmt.Errorf("connection has not been established")
+	}
+
 	// Set the write deadline to the configured write timeout.
 	if err := h.conn.SetWriteDeadline(time.Now().Add(h.cfg.WriteTimeout)); err != nil {
 		return err
@@ -121,6 +129,10 @@ func (h *WebSocketConnHandlerImpl) Write(message []byte) error {
 func (h *WebSocketConnHandlerImpl) Close() error {
 	h.Lock()
 	defer h.Unlock()
+
+	if h.conn == nil {
+		return fmt.Errorf("connection has not been established")
+	}
 
 	if h.conn == nil {
 		return fmt.Errorf("connection has not been established")
