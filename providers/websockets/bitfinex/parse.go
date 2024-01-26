21745010023d95cc60a6c4b75f4e1b969eff2ed4
aljo242 <alex@skip.money>
2024-01-26 11:33:29 -0500

    fix

--- providers/websockets/bitfinex/parse.go
+++ providers/websockets/bitfinex/parse.go
@@ -1,18 +1,92 @@
 package bitfinex
 
 import (
+	"encoding/json"
+	"fmt"
+	"math/big"
+	"time"
+
+	"go.uber.org/zap"
+
+	"github.com/skip-mev/slinky/pkg/math"
 	"github.com/skip-mev/slinky/providers/base/websocket/handlers"
+	providertypes "github.com/skip-mev/slinky/providers/types"
+	oracletypes "github.com/skip-mev/slinky/x/oracle/types"
 )
 
+// parseSubscribedMessage updates the channel map for a subscribed message.
 func (h *WebsocketDataHandler) parseSubscribedMessage(
 	msg SubscribedMessage,
 ) error {
 	return h.UpdateChannelMap(msg.ChannelID, msg.Pair)
 }
 
+// parseErrorMessage returns the proper error code from an error message.
 func (h *WebsocketDataHandler) parseErrorMessage(
 	msg ErrorMessage,
 ) ([]handlers.WebsocketEncodedMessage, error) {
 	e := ErrorCode(msg.Code)
 	return nil, e.Error()
+}
+
+// handleStream handles a data stream sent from the peer.
+func (h *WebsocketDataHandler) handleStream(
+	message []byte,
+) (providertypes.GetResponse[oracletypes.CurrencyPair, *big.Int], error) {
+	var (
+		baseStream []interface{}
+		resolved   = make(map[oracletypes.CurrencyPair]providertypes.Result[*big.Int])
+		unResolved = make(map[oracletypes.CurrencyPair]error)
+	)
+
+	// Attempt to unmarshal the message into a base message. This is used to determine the type
+	// of message that was received.
+	if err := json.Unmarshal(message, &baseStream); err != nil {
+		h.logger.Debug("unable to unmarshal message into base struct", zap.Error(err))
+		return providertypes.NewGetResponse[oracletypes.CurrencyPair, *big.Int](resolved, unResolved), err
+	}
+
+	if len(baseStream) != ExpectedBaseStreamLength {
+		h.logger.Error("invalid length of stream data received. must be 2", zap.Any("data", baseStream), zap.Int("len", len(baseStream)))
+		return providertypes.NewGetResponse[oracletypes.CurrencyPair, *big.Int](resolved, unResolved), fmt.Errorf("invalid length of stream data received. must be %d.  stream: %v. len: %d",
+			ExpectedBaseStreamLength,
+			baseStream,
+			len(baseStream),
+		)
+	}
+
+	// first element is always channel id
+	channelID := int(baseStream[0].(float64))
+	market, ok := h.channelMap[channelID]
+	if !ok {
+		h.logger.Error("received stream for unknown channel id", zap.Int("channel_id", channelID))
+		return providertypes.NewGetResponse[oracletypes.CurrencyPair, *big.Int](resolved, unResolved), fmt.Errorf("received stream for unknown channel id %v", channelID)
+	}
+
+	cp := market.CurrencyPair
+	h.logger.Debug("received stream", zap.Int("channel_id", channelID), zap.String("market", cp.String()))
+
+	// check if it is a heartbeat
+	hbID, ok := baseStream[1].(string)
+	if ok && hbID == IDHeartbeat {
+
+		h.logger.Debug("received heartbeat", zap.Int("channel_id", channelID), zap.String("pair", market.Ticker))
+		return providertypes.NewGetResponse[oracletypes.CurrencyPair, *big.Int](resolved, unResolved), nil
+
+	}
+
+	// if it is not a string, it is a stream update
+	dataArr, ok := baseStream[1].([]interface{})
+	if !ok || len(dataArr) != ExpectedDataStreamLength {
+		err := fmt.Errorf("unknown data: %v, len: %d", baseStream[1], len(dataArr))
+		unResolved[cp] = err
+		return providertypes.NewGetResponse[oracletypes.CurrencyPair, *big.Int](resolved, unResolved), err
+	}
+
+	lastPrice := dataArr[6]
+	// Convert the price to a big int.
+	price := math.Float64ToBigInt(lastPrice.(float64), cp.Decimals())
+	resolved[cp] = providertypes.NewResult[*big.Int](price, time.Now().UTC())
+
+	return providertypes.NewGetResponse[oracletypes.CurrencyPair, *big.Int](resolved, unResolved), nil
 }
--- providers/websockets/bitfinex/ws_data_handler.go
+++ providers/websockets/bitfinex/ws_data_handler.go
@@ -4,11 +4,8 @@ import (
 	"encoding/json"
 	"fmt"
 	"math/big"
-	"time"
 
 	"go.uber.org/zap"
-
-	"github.com/skip-mev/slinky/pkg/math"
 
 	"github.com/skip-mev/slinky/oracle/config"
 	providertypes "github.com/skip-mev/slinky/providers/types"
@@ -29,17 +26,6 @@ type WebsocketDataHandler struct {
 
 	// config is the config for the BitFinex web socket API.
 	cfg config.ProviderConfig
-}
-
-// UpdateChannelMap updates the internal map from
-func (h *WebsocketDataHandler) UpdateChannelMap(channelID int, ticker string) error {
-	market, ok := h.cfg.Market.TickerToMarketConfigs[ticker]
-	if !ok {
-		return fmt.Errorf("unable to find market for currency pair: %s", ticker)
-	}
-
-	h.channelMap[channelID] = market
-	return nil
 }
 
 // NewWebSocketDataHandler returns a new WebSocketDataHandler implementation for BitFinex
@@ -135,68 +121,6 @@ func (h *WebsocketDataHandler) HandleMessage(
 	}
 }
 
-// handleStream handles a data stream sent from the peer.
-func (h *WebsocketDataHandler) handleStream(
-	message []byte,
-) (providertypes.GetResponse[oracletypes.CurrencyPair, *big.Int], error) {
-	var (
-		baseStream []interface{}
-		resolved   = make(map[oracletypes.CurrencyPair]providertypes.Result[*big.Int])
-		unResolved = make(map[oracletypes.CurrencyPair]error)
-	)
-
-	// Attempt to unmarshal the message into a base message. This is used to determine the type
-	// of message that was received.
-	if err := json.Unmarshal(message, &baseStream); err != nil {
-		h.logger.Debug("unable to unmarshal message into base struct", zap.Error(err))
-		return providertypes.NewGetResponse[oracletypes.CurrencyPair, *big.Int](resolved, unResolved), err
-	}
-
-	if len(baseStream) != ExpectedBaseStreamLength {
-		h.logger.Error("invalid length of stream data received. must be 2", zap.Any("data", baseStream), zap.Int("len", len(baseStream)))
-		return providertypes.NewGetResponse[oracletypes.CurrencyPair, *big.Int](resolved, unResolved), fmt.Errorf("invalid length of stream data received. must be %d.  stream: %v. len: %d",
-			ExpectedBaseStreamLength,
-			baseStream,
-			len(baseStream),
-		)
-	}
-
-	// first element is always channel id
-	channelID := int(baseStream[0].(float64))
-	market, ok := h.channelMap[channelID]
-	if !ok {
-		h.logger.Error("received stream for unknown channel id", zap.Int("channel_id", channelID))
-		return providertypes.NewGetResponse[oracletypes.CurrencyPair, *big.Int](resolved, unResolved), fmt.Errorf("received stream for unknown channel id %v", channelID)
-	}
-
-	cp := market.CurrencyPair
-	h.logger.Debug("received stream", zap.Int("channel_id", channelID), zap.String("market", cp.String()))
-
-	// check if it is a heartbeat
-	hbID, ok := baseStream[1].(string)
-	if ok && hbID == IDHeartbeat {
-
-		h.logger.Debug("received heartbeat", zap.Int("channel_id", channelID), zap.String("pair", market.Ticker))
-		return providertypes.NewGetResponse[oracletypes.CurrencyPair, *big.Int](resolved, unResolved), nil
-
-	}
-
-	// if it is not a string, it is a stream update
-	dataArr, ok := baseStream[1].([]interface{})
-	if !ok || len(dataArr) != ExpectedDataStreamLength {
-		err := fmt.Errorf("unknown data: %v, len: %d", baseStream[1], len(dataArr))
-		unResolved[cp] = err
-		return providertypes.NewGetResponse[oracletypes.CurrencyPair, *big.Int](resolved, unResolved), err
-	}
-
-	lastPrice := dataArr[6]
-	// Convert the price to a big int.
-	price := math.Float64ToBigInt(lastPrice.(float64), cp.Decimals())
-	resolved[cp] = providertypes.NewResult[*big.Int](price, time.Now().UTC())
-
-	return providertypes.NewGetResponse[oracletypes.CurrencyPair, *big.Int](resolved, unResolved), nil
-}
-
 // CreateMessages is used to create an initial subscription message to send to the data provider.
 // Only the currency pairs that are specified in the config are subscribed to. The only channel
 // that is subscribed to is the index tickers channel - which supports spot markets.
@@ -228,4 +152,15 @@ func (h *WebsocketDataHandler) CreateMessages(
 // HeartBeatMessages is not used for BitFinex.
 func (h *WebsocketDataHandler) HeartBeatMessages() ([]handlers.WebsocketEncodedMessage, error) {
 	return nil, nil
+}
+
+// UpdateChannelMap updates the internal map from
+func (h *WebsocketDataHandler) UpdateChannelMap(channelID int, ticker string) error {
+	market, ok := h.cfg.Market.TickerToMarketConfigs[ticker]
+	if !ok {
+		return fmt.Errorf("unable to find market for currency pair: %s", ticker)
+	}
+
+	h.channelMap[channelID] = market
+	return nil
 }
