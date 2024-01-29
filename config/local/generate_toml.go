113862d4f5104c1ed6bfd18e0209f13215aebfb7
aljo242 <alex@skip.money>
2024-01-29 10:19:31 -0500

    fix cfg

--- config/local/generate_toml.go
+++ config/local/generate_toml.go
@@ -12,7 +12,18 @@ import (
 	"github.com/BurntSushi/toml"
 
 	"github.com/skip-mev/slinky/oracle/config"
+	"github.com/skip-mev/slinky/providers/apis/binance"
+	coinbaseapi "github.com/skip-mev/slinky/providers/apis/coinbase"
+	"github.com/skip-mev/slinky/providers/apis/coingecko"
+	"github.com/skip-mev/slinky/providers/websockets/bitfinex"
+	"github.com/skip-mev/slinky/providers/websockets/bitstamp"
+	"github.com/skip-mev/slinky/providers/websockets/bybit"
+	coinbasews "github.com/skip-mev/slinky/providers/websockets/coinbase"
+	"github.com/skip-mev/slinky/providers/websockets/cryptodotcom"
 	"github.com/skip-mev/slinky/providers/websockets/gate"
+	"github.com/skip-mev/slinky/providers/websockets/kraken"
+	"github.com/skip-mev/slinky/providers/websockets/kucoin"
+	"github.com/skip-mev/slinky/providers/websockets/okx"
 	oracletypes "github.com/skip-mev/slinky/x/oracle/types"
 )
 
@@ -55,9 +66,72 @@ var LocalConfig = config.OracleConfig{
 		// all of the currency pairs. Before adding a new market to the oracle, ensure that
 		// the provider supports fetching data for the currency pair.
 		{
+			Name:   binance.Name,
+			API:    binance.DefaultUSAPIConfig,
+			Market: binance.DefaultMarketConfig,
+		},
+		{
+			Name:   coinbaseapi.Name,
+			API:    coinbaseapi.DefaultAPIConfig,
+			Market: coinbaseapi.DefaultMarketConfig,
+		},
+		{
+			Name:   coingecko.Name,
+			API:    coingecko.DefaultAPIConfig,
+			Market: coingecko.DefaultMarketConfig,
+		},
+		// -----------------------------------------------------------	//
+		// ---------------------Start WebSocket Providers--------------	//
+		// -----------------------------------------------------------	//
+		//
+		// NOTE: Some of the provider's are only capable of fetching data for a subset of
+		// all currency pairs. Before adding a new market to the oracle, ensure that
+		// the provider supports fetching data for the currency pair.
+		{
+			Name:      bitfinex.Name,
+			WebSocket: bitfinex.DefaultWebSocketConfig,
+			Market:    bitfinex.DefaultMarketConfig,
+		},
+		{
+			Name:      bitstamp.Name,
+			WebSocket: bitstamp.DefaultWebSocketConfig,
+			Market:    bitstamp.DefaultMarketConfig,
+		},
+		{
+			Name:      bybit.Name,
+			WebSocket: bybit.DefaultWebSocketConfig,
+			Market:    bybit.DefaultMarketCOnfig,
+		},
+		{
+			Name:      coinbasews.Name,
+			WebSocket: coinbasews.DefaultWebSocketConfig,
+			Market:    coinbasews.DefaultMarketConfig,
+		},
+		{
+			Name:      cryptodotcom.Name,
+			WebSocket: cryptodotcom.DefaultWebSocketConfig,
+			Market:    cryptodotcom.DefaultMarketConfig,
+		},
+		{
 			Name:      gate.Name,
 			WebSocket: gate.DefaultWebSocketConfig,
 			Market:    gate.DefaultMarketConfig,
+		},
+		{
+			Name:      kraken.Name,
+			WebSocket: kraken.DefaultWebSocketConfig,
+			Market:    kraken.DefaultMarketConfig,
+		},
+		{
+			Name:      kucoin.Name,
+			WebSocket: kucoin.DefaultWebSocketConfig,
+			API:       kucoin.DefaultAPIConfig,
+			Market:    kucoin.DefaultMarketConfig,
+		},
+		{
+			Name:      okx.Name,
+			WebSocket: okx.DefaultWebSocketConfig,
+			Market:    okx.DefaultMarketConfig,
 		},
 	},
 }
