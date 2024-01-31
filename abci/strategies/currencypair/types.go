294768e4f8f1ac81e0f3eedcdbbce6becbe4fe40
Nikhil Vasan <nikhil@skip.money>
2024-01-31 08:32:10 -0800

    remove GetAllCurrencyPairs from the currencypair OracleKeeper iface

--- abci/strategies/currencypair/types.go
+++ abci/strategies/currencypair/types.go
@@ -12,7 +12,6 @@ import (
 //
 //go:generate mockery --name OracleKeeper --filename mock_oracle_keeper.go
 type OracleKeeper interface {
-	GetAllCurrencyPairs(ctx sdk.Context) []oracletypes.CurrencyPair
 	GetCurrencyPairFromID(ctx sdk.Context, id uint64) (cp oracletypes.CurrencyPair, found bool)
 	GetIDForCurrencyPair(ctx sdk.Context, cp oracletypes.CurrencyPair) (uint64, bool)
 	GetPriceForCurrencyPair(ctx sdk.Context, cp oracletypes.CurrencyPair) (oracletypes.QuotePrice, error)
