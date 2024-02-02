59a87c002ad145b9999eafc4bf196efecc8765c6
aljo242 <alex@skip.money>
2024-02-02 12:04:18 -0500

    test

--- pkg/math/math_test.go
+++ pkg/math/math_test.go
@@ -2,6 +2,7 @@ package math_test
 
 import (
 	"math/big"
+	"strconv"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -13,38 +14,86 @@ func TestMin(t *testing.T) {
 	t.Parallel()
 
 	testCases := []struct {
-		name string
-		vals []int
-		min  int
+		name     string
+		vals     []int
+		expected int
 	}{
 		{
-			name: "one value",
-			vals: []int{1},
-			min:  1,
+			name:     "one value",
+			vals:     []int{1},
+			expected: 1,
 		},
 		{
-			name: "two values",
-			vals: []int{1, 2},
-			min:  1,
+			name:     "two values",
+			vals:     []int{1, 2},
+			expected: 1,
 		},
 		{
-			name: "three values",
-			vals: []int{1, 2, 3},
-			min:  1,
+			name:     "three values",
+			vals:     []int{1, 2, 3},
+			expected: 1,
 		},
 		{
-			name: "five values, negative",
-			vals: []int{1, 2, 3, 4, -5},
-			min:  -5,
+			name:     "five values, negative",
+			vals:     []int{1, 2, 3, 4, -5},
+			expected: -5,
 		},
 	}
 
 	for _, tc := range testCases {
 		t.Run(tc.name, func(t *testing.T) {
-			min := math.Min(tc.vals...)
-			if min != tc.min {
-				t.Errorf("expected %d, got %d", tc.min, min)
+			got := math.Min(tc.vals...)
+			if got != tc.expected {
+				t.Errorf("expected %d, got %d", tc.expected, got)
 			}
+		})
+	}
+}
+
+func TestFloat64StringToBigInt(t *testing.T) {
+	testCases := []struct {
+		name     string
+		input    string
+		base     int
+		expected *big.Int
+	}{
+		{
+			"zero",
+			strconv.FormatFloat(0, 'g', -1, 64),
+			6,
+			big.NewInt(0),
+		},
+		{
+			"one",
+			strconv.FormatFloat(1, 'g', -1, 64),
+			6,
+			big.NewInt(1000000),
+		},
+		{
+			"one point one",
+			strconv.FormatFloat(1.1, 'g', -1, 64),
+			6,
+			big.NewInt(1100000),
+		},
+		{
+			"many decimal points",
+			strconv.FormatFloat(1.123456789, 'g', -1, 64),
+			6,
+			big.NewInt(1123456),
+		},
+		{
+			"random big number with many decimal points",
+			strconv.FormatFloat(123456789.123456789, 'g', -1, 64),
+			6,
+			big.NewInt(123456789123456),
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			result, err := math.Float64StringToBigInt(tc.input, tc.base)
+			require.NoError(t, err)
+			require.Equal(t, tc.expected, result)
 		})
 	}
 }
@@ -91,6 +140,54 @@ func TestFloat64ToBigInt(t *testing.T) {
 	for _, tc := range testCases {
 		t.Run(tc.name, func(t *testing.T) {
 			result := math.Float64ToBigInt(tc.input, tc.base)
+			require.Equal(t, tc.expected, result)
+		})
+	}
+}
+
+func TestBigFloatToBigInt(t *testing.T) {
+	testCases := []struct {
+		name     string
+		input    *big.Float
+		base     int
+		expected *big.Int
+	}{
+		{
+			"zero",
+			big.NewFloat(0),
+			6,
+			big.NewInt(0),
+		},
+		{
+			"one",
+			big.NewFloat(1),
+			6,
+			big.NewInt(1000000),
+		},
+		{
+			"one point one",
+			big.NewFloat(1.1),
+
+			6,
+			big.NewInt(1100000),
+		},
+		{
+			"many decimal points",
+			big.NewFloat(1.123456789),
+			6,
+			big.NewInt(1123456),
+		},
+		{
+			"random big number with many decimal points",
+			big.NewFloat(123456789.123456789),
+			6,
+			big.NewInt(123456789123456),
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			result := math.BigFloatToBigInt(tc.input, tc.base)
 			require.Equal(t, tc.expected, result)
 		})
 	}
