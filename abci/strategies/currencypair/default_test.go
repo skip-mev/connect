5a19c959e3d14f61b9d31937ec430350e3f9a2bd
aljo242 <alex@skip.money>
2024-02-02 16:41:25 -0500

    revert

--- abci/strategies/currencypair/default_test.go
+++ abci/strategies/currencypair/default_test.go
@@ -21,20 +21,20 @@ func TestDefaultCurrencyPairStrategyID(t *testing.T) {
 
 	// test that the strategy returns IDs from the oracle module
 	t.Run("test getting ids with two currency-pairs in module-state", func(t *testing.T) {
-		// expect the first currency-pair to have Ticker 0
+		// expect the first currency-pair to have ID 0
 		ok.On("GetIDForCurrencyPair", ctx, "BTC/USD").Return(uint64(0), true)
 		id, err := strategy.ID(ctx, oracletypes.NewCurrencyPair("BTC", "USD", oracletypes.DefaultDecimals))
 		require.NoError(t, err)
 		require.Equal(t, uint64(0), id)
 
-		// expect the second currency-pair to have Ticker 1
+		// expect the second currency-pair to have ID 1
 		ok.On("GetIDForCurrencyPair", ctx, "USD/ETH").Return(uint64(1), true)
 		id, err = strategy.ID(ctx, oracletypes.NewCurrencyPair("USD", "ETH", oracletypes.EthereumDecimals))
 		require.NoError(t, err)
 		require.Equal(t, uint64(1), id)
 	})
 
-	// test that if a currency-pair does not have an Ticker w/ x/oracle, a failure is returned
+	// test that if a currency-pair does not have an ID w/ x/oracle, a failure is returned
 	t.Run("expect error when currency-pair not found in module-state", func(t *testing.T) {
 		// expect an error when querying for a currency-pair not in module-state
 		ok.On("GetIDForCurrencyPair", ctx, "ETH/BTC").Return(uint64(0), false)
@@ -52,20 +52,20 @@ func TestDefaultCurrencyPairStrategyFromID(t *testing.T) {
 
 	// test that the strategy returns IDs from the oracle module
 	t.Run("test getting ids with two currency-pairs in module-state", func(t *testing.T) {
-		// expect the first currency-pair to have Ticker 0
+		// expect the first currency-pair to have ID 0
 		ok.On("GetCurrencyPairFromID", ctx, uint64(0)).Return(oracletypes.NewCurrencyPair("BTC", "USD", oracletypes.DefaultDecimals), true)
 		cp, err := strategy.FromID(ctx, uint64(0))
 		require.NoError(t, err)
 		require.Equal(t, oracletypes.NewCurrencyPair("BTC", "USD", oracletypes.DefaultDecimals), cp)
 
-		// expect the second currency-pair to have Ticker 1
+		// expect the second currency-pair to have ID 1
 		ok.On("GetCurrencyPairFromID", ctx, uint64(1)).Return(oracletypes.NewCurrencyPair("USD", "ETH", oracletypes.EthereumDecimals), true)
 		cp, err = strategy.FromID(ctx, uint64(1))
 		require.NoError(t, err)
 		require.Equal(t, oracletypes.NewCurrencyPair("USD", "ETH", oracletypes.EthereumDecimals), cp)
 	})
 
-	// test that if a currency-pair does not have an Ticker w/ x/oracle, a failure is returned
+	// test that if a currency-pair does not have an ID w/ x/oracle, a failure is returned
 	t.Run("expect error when currency-pair not found in module-state", func(t *testing.T) {
 		// expect an error when querying for a currency-pair not in module-state
 		ok.On("GetCurrencyPairFromID", ctx, uint64(2)).Return(oracletypes.CurrencyPair{}, false)
