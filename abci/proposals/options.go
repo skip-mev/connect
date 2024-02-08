b3277d4878f92ad5fb8d5dd416c8e4d96d860349
Nikhil Vasan <97126437+nivasan1@users.noreply.github.com>
2024-02-08 07:11:26 -0800

    Update abci/proposals/options.go
    Co-authored-by: Alex Johnson <alex@skip.money>

--- abci/proposals/options.go
+++ abci/proposals/options.go
@@ -3,8 +3,8 @@ package proposals
 // Option is a function that enables optional configuration of the ProposalHandler.
 type Option func(*ProposalHandler)
 
-// RetainOracleDataInWrappedProposalHandler returns an Option that configs the ProposalHandler
-// to pass the injected extend-commit-info to the wrapped proposal handler.
+// RetainOracleDataInWrappedProposalHandler returns an Option that configures the
+// ProposalHandler to pass the injected extend-commit-info to the wrapped proposal handler.
 func RetainOracleDataInWrappedProposalHandler() Option {
 	return func(p *ProposalHandler) {
 		p.retainOracleDataInWrappedHandler = true
