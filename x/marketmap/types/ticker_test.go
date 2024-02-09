93a0bd7b1d7b2ba30c3227270e5532cbe934cb21
aljo242 <alex@skip.money>
2024-02-09 12:16:09 -0500

    format

--- x/marketmap/types/ticker_test.go
+++ x/marketmap/types/ticker_test.go
@@ -3,8 +3,9 @@ package types_test
 import (
 	"testing"
 
-	"github.com/skip-mev/slinky/x/marketmap/types"
 	"github.com/stretchr/testify/require"
+
+	"github.com/skip-mev/slinky/x/marketmap/types"
 )
 
 func TestTicker(t *testing.T) {
