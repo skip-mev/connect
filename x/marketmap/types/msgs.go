768cf3c352b376abcc0d32b9c6454bf68c9d7569
aljo242 <alex@skip.money>
2024-02-13 08:46:16 -0500

    signer

--- x/marketmap/types/msgs.go
+++ x/marketmap/types/msgs.go
@@ -3,3 +3,12 @@ package types
 import sdk "github.com/cosmos/cosmos-sdk/types"
 
 var _ sdk.Msg = &MsgCreateMarket{}
+
+// ValidateBasic determines whether the information in the message is formatted correctly, specifically
+// whether the signer is a valid acc-address.
+func (m *MsgCreateMarket) ValidateBasic() error {
+	// validate signer address
+	_, err := sdk.AccAddressFromBech32(m.Signer)
+
+	return err
+}
