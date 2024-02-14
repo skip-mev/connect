1ab4daf073ca7cfc9df3178b6fc76275e551cef8
David Terpay <david.terpay@gmail.com>
2024-02-14 17:34:29 -0500

    crypto.dom

--- providers/websockets/cryptodotcom/parse.go
+++ providers/websockets/cryptodotcom/parse.go
@@ -8,46 +8,46 @@ import (
 	"go.uber.org/zap"
 
 	"github.com/skip-mev/slinky/pkg/math"
-	slinkytypes "github.com/skip-mev/slinky/pkg/types"
 	providertypes "github.com/skip-mev/slinky/providers/types"
+	mmtypes "github.com/skip-mev/slinky/x/marketmap/types"
 )
 
 // parseInstrumentMessage is used to parse an instrument message received from the Crypto.com
 // websocket API. This message contains the latest price data for a set of instruments.
-func (h *WebSocketDataHandler) parseInstrumentMessage(
+func (h *WebSocketHandler) parseInstrumentMessage(
 	msg InstrumentResponseMessage,
-) (providertypes.GetResponse[slinkytypes.CurrencyPair, *big.Int], error) {
+) (providertypes.GetResponse[mmtypes.Ticker, *big.Int], error) {
 	var (
-		resolved    = make(map[slinkytypes.CurrencyPair]providertypes.Result[*big.Int])
-		unresolved  = make(map[slinkytypes.CurrencyPair]error)
+		resolved    = make(map[mmtypes.Ticker]providertypes.Result[*big.Int])
+		unresolved  = make(map[mmtypes.Ticker]error)
 		instruments = msg.Result.Data
 	)
 
 	// If the response contained no instrument data, return an error.
 	if len(instruments) == 0 {
-		return providertypes.NewGetResponse[slinkytypes.CurrencyPair, *big.Int](resolved, unresolved),
+		return providertypes.NewGetResponse[mmtypes.Ticker, *big.Int](resolved, unresolved),
 			fmt.Errorf("no instrument data was returned")
 	}
 
 	// Iterate through each market and attempt to parse the price.
+	inverted := h.market.Invert()
 	for _, instrument := range instruments {
 		// If we don't have a mapping for the instrument, return an error. This is likely a configuration
 		// error.
-		market, ok := h.cfg.Market.TickerToMarketConfigs[instrument.Name]
+		market, ok := inverted[instrument.Name]
 		if !ok {
 			h.logger.Error("failed to find currency pair for instrument", zap.String("instrument", instrument.Name))
 			continue
 		}
 
 		// Attempt to parse the price.
-		cp := market.CurrencyPair
-		if price, err := math.Float64StringToBigInt(instrument.LatestTradePrice, cp.Decimals()); err != nil {
-			unresolved[cp] = fmt.Errorf("failed to parse price %s: %w", instrument.LatestTradePrice, err)
+		if price, err := math.Float64StringToBigInt(instrument.LatestTradePrice, market.Ticker.Decimals); err != nil {
+			unresolved[market.Ticker] = fmt.Errorf("failed to parse price %s: %w", instrument.LatestTradePrice, err)
 		} else {
-			resolved[cp] = providertypes.NewResult[*big.Int](price, time.Now().UTC())
+			resolved[market.Ticker] = providertypes.NewResult[*big.Int](price, time.Now().UTC())
 		}
 
 	}
 
-	return providertypes.NewGetResponse[slinkytypes.CurrencyPair, *big.Int](resolved, unresolved), nil
+	return providertypes.NewGetResponse[mmtypes.Ticker, *big.Int](resolved, unresolved), nil
 }
--- providers/websockets/cryptodotcom/ws_data_handler.go
+++ providers/websockets/cryptodotcom/ws_data_handler.go
@@ -8,42 +8,55 @@ import (
 	"go.uber.org/zap"
 
 	"github.com/skip-mev/slinky/oracle/config"
-	slinkytypes "github.com/skip-mev/slinky/pkg/types"
 	"github.com/skip-mev/slinky/providers/base/websocket/handlers"
 	providertypes "github.com/skip-mev/slinky/providers/types"
+	mmtypes "github.com/skip-mev/slinky/x/marketmap/types"
 )
 
-var _ handlers.WebSocketDataHandler[slinkytypes.CurrencyPair, *big.Int] = (*WebSocketDataHandler)(nil)
+var _ handlers.WebSocketDataHandler[mmtypes.Ticker, *big.Int] = (*WebSocketHandler)(nil)
 
-// WebSocketDataHandler implements the WebSocketDataHandler interface. This is used to
+// WebSocketHandler implements the WebSocketDataHandler interface. This is used to
 // handle messages received from the Crypto.com websocket API.
-type WebSocketDataHandler struct {
+type WebSocketHandler struct {
 	logger *zap.Logger
 
-	// config is the config for the Crypto.com websocket API.
-	cfg config.ProviderConfig
+	// market is the config for the Crypto.com API.
+	market mmtypes.MarketConfig
+	// ws is the config for the Crypto.com websocket.
+	ws config.WebSocketConfig
 }
 
 // NewWebSocketDataHandler returns a new WebSocketDataHandler implementation for Crypto.com.
 func NewWebSocketDataHandler(
 	logger *zap.Logger,
-	cfg config.ProviderConfig,
-) (handlers.WebSocketDataHandler[slinkytypes.CurrencyPair, *big.Int], error) {
-	if err := cfg.ValidateBasic(); err != nil {
+	marketCfg mmtypes.MarketConfig,
+	wsCfg config.WebSocketConfig,
+) (handlers.WebSocketDataHandler[mmtypes.Ticker, *big.Int], error) {
+	if err := marketCfg.ValidateBasic(); err != nil {
 		return nil, fmt.Errorf("invalid provider config %w", err)
 	}
 
-	if !cfg.WebSocket.Enabled {
-		return nil, fmt.Errorf("websocket is not enabled for provider %s", cfg.Name)
+	if marketCfg.Name != Name {
+		return nil, fmt.Errorf("expected provider config name %s, got %s", Name, marketCfg.Name)
 	}
 
-	if cfg.Name != Name {
-		return nil, fmt.Errorf("invalid provider name %s", cfg.Name)
+	if wsCfg.Name != Name {
+		return nil, fmt.Errorf("expected websocket config name %s, got %s", Name, wsCfg.Name)
 	}
 
-	return &WebSocketDataHandler{
-		cfg:    cfg,
-		logger: logger.With(zap.String("web_socket_data_handler", Name)),
+	if !wsCfg.Enabled {
+		return nil, fmt.Errorf("websocket config for %s is not enabled", Name)
+
+	}
+
+	if err := wsCfg.ValidateBasic(); err != nil {
+		return nil, fmt.Errorf("invalid websocket config %w", err)
+	}
+
+	return &WebSocketHandler{
+		logger: logger,
+		market: marketCfg,
+		ws:     wsCfg,
 	}, nil
 }
 
@@ -52,12 +65,12 @@ func NewWebSocketDataHandler(
 // a heartbeat response message must be sent back to the Crypto.com websocket API, otherwise
 // the connection will be closed. If a subscribe message is received, the message must be parsed
 // and a response must be returned. No update message is required for subscribe messages.
-func (h *WebSocketDataHandler) HandleMessage(
+func (h *WebSocketHandler) HandleMessage(
 	message []byte,
-) (providertypes.GetResponse[slinkytypes.CurrencyPair, *big.Int], []handlers.WebsocketEncodedMessage, error) {
+) (providertypes.GetResponse[mmtypes.Ticker, *big.Int], []handlers.WebsocketEncodedMessage, error) {
 	var (
 		msg  InstrumentResponseMessage
-		resp providertypes.GetResponse[slinkytypes.CurrencyPair, *big.Int]
+		resp providertypes.GetResponse[mmtypes.Ticker, *big.Int]
 	)
 
 	if err := json.Unmarshal(message, &msg); err != nil {
@@ -98,31 +111,26 @@ func (h *WebSocketDataHandler) HandleMessage(
 }
 
 // CreateMessages is used to create a message to send to the data provider. This is used to
-// subscribe to the given currency pairs. This is called when the connection to the data
-// provider is first established.
-func (h *WebSocketDataHandler) CreateMessages(
-	cps []slinkytypes.CurrencyPair,
+// subscribe to the given tickers. This is called when the connection to the data provider
+// is first established.
+func (h *WebSocketHandler) CreateMessages(
+	tickers []mmtypes.Ticker,
 ) ([]handlers.WebsocketEncodedMessage, error) {
 	instruments := make([]string, 0)
 
-	// Iterate through each currency pair and get the instrument name. The instrument name
-	// corresponds to the perpetual contract name on the Crypto.com websocket API. This will
-	// only subscribe to price feeds that are configured in the config file.
-	for _, cp := range cps {
-		market, ok := h.cfg.Market.CurrencyPairToMarketConfigs[cp.String()]
+	for _, ticker := range tickers {
+		market, ok := h.market.TickerConfigs[ticker.String()]
 		if !ok {
-			h.logger.Debug("no market configuration for currency pair", zap.String("currency_pair", cp.String()))
-			continue
+			return nil, fmt.Errorf("ticker not found in market configs %s", ticker.String())
 		}
 
-		instruments = append(instruments, fmt.Sprintf(TickerChannel, market.Ticker))
+		instruments = append(instruments, fmt.Sprintf(TickerChannel, market.OffChainTicker))
 	}
 
-	h.logger.Debug("subscribing to instruments", zap.Strings("instruments", instruments))
 	return NewInstrumentMessage(instruments)
 }
 
 // HeartBeatMessages is not used for Crypto.com.
-func (h *WebSocketDataHandler) HeartBeatMessages() ([]handlers.WebsocketEncodedMessage, error) {
+func (h *WebSocketHandler) HeartBeatMessages() ([]handlers.WebsocketEncodedMessage, error) {
 	return nil, nil
 }
