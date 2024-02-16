a0cab3664c96b278b2836a1c05ca4241aed28920
aljo242 <alex@skip.money>
2024-02-15 19:22:06 -0500

    types

--- x/marketmap/types/params.go
+++ x/marketmap/types/params.go
@@ -8,17 +8,24 @@ import (
 	govtypes "github.com/cosmos/cosmos-sdk/x/gov/types"
 )
 
+const (
+	// DefaultVersion is the default value for the Version Param.
+	DefaultVersion = 0
+)
+
 // DefaultParams returns default marketmap parameters.
 func DefaultParams() Params {
 	return Params{
 		MarketAuthority: authtypes.NewModuleAddress(govtypes.ModuleName).String(),
+		Version:         DefaultVersion,
 	}
 }
 
 // NewParams returns a new Params instance.
-func NewParams(authority string) Params {
+func NewParams(authority string, version uint64) Params {
 	return Params{
 		MarketAuthority: authority,
+		Version:         version,
 	}
 }
 
