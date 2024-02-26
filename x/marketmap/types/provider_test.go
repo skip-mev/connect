da40e7c144bdf56c18d31aa0ad0a3a75522b95fa
aljo242 <alex@skip.money>
2024-02-26 12:08:33 -0500

    tidy

--- x/marketmap/types/provider_test.go
+++ x/marketmap/types/provider_test.go
@@ -1,3 +1,34 @@
 package types_test
 
-// TODO finish
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+
+	"github.com/skip-mev/slinky/x/marketmap/types"
+)
+
+func TestProviderConfigValidateBasic(t *testing.T) {
+	t.Run("valid config - pass", func(t *testing.T) {
+		pc := types.ProviderConfig{
+			Name:           "mexc",
+			OffChainTicker: "ticker",
+		}
+		require.NoError(t, pc.ValidateBasic())
+	})
+	t.Run("invalid name - fail", func(t *testing.T) {
+		pc := types.ProviderConfig{
+			Name:           "",
+			OffChainTicker: "ticker",
+		}
+		require.Error(t, pc.ValidateBasic())
+	})
+	t.Run("valid offchain ticker - fail", func(t *testing.T) {
+		pc := types.ProviderConfig{
+			Name:           "mexc",
+			OffChainTicker: "",
+		}
+		require.Error(t, pc.ValidateBasic())
+	})
+
+}
