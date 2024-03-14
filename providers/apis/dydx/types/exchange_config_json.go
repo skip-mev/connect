6eae29af7272886912f9feeee1ce7702ca54f661
David Terpay <david.terpay@gmail.com>
2024-03-14 19:38:35 -0400

    lints

--- providers/apis/dydx/types/exchange_config_json.go
+++ providers/apis/dydx/types/exchange_config_json.go
@@ -8,14 +8,14 @@ package types
 // `ExchangeMarketConfig` objects, which are keyed by exchange id. These objects are not kept
 // past the time the `GetAllMarketParams` API response is parsed, and do not contain an id
 // because the id is expected to be known at the time the object is in use.
-type ExchangeConfigJson struct {
+type ExchangeConfigJson struct { //nolint
 	Exchanges []ExchangeMarketConfigJson `json:"exchanges"`
 }
 
 // ExchangeMarketConfigJson captures per-exchange information for resolving a market, including
 // the ticker and conversion details. It demarshals JSON parameters from the chain for a
 // particular market on a specific exchange.
-type ExchangeMarketConfigJson struct {
+type ExchangeMarketConfigJson struct { //nolint
 	ExchangeName   string `json:"exchangeName"`
 	Ticker         string `json:"ticker"`
 	AdjustByMarket string `json:"adjustByMarket,omitempty"`
