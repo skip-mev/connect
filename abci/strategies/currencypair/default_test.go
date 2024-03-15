8cc04f60013bc0b9b3e799d8787c58bf258d9594
aljo242 <alex@skip.money>
2024-03-15 15:06:41 -0400

    test

--- abci/strategies/currencypair/default_test.go
+++ abci/strategies/currencypair/default_test.go
@@ -18,7 +18,7 @@ var (
 	ethbtc = slinkytypes.NewCurrencyPair("ETH", "BTC")
 )
 
-func TestDefaultCurrencyPairStrategyIDNoCache(t *testing.T) {
+func TestDefaultCurrencyPairStrategyID(t *testing.T) {
 	ok := mocks.NewOracleKeeper(t)
 
 	ctx := sdk.Context{}
@@ -49,76 +49,12 @@ func TestDefaultCurrencyPairStrategyIDNoCache(t *testing.T) {
 	})
 }
 
-func TestDefaultCurrencyPairStrategyIDWithCache(t *testing.T) {
-	ok := mocks.NewOracleKeeper(t)
-
-	ctx := sdk.Context{}
-
-	opts := strategies.WithCache()
-	strategy := strategies.NewDefaultCurrencyPairStrategy(ok, opts)
-
-	// test that the strategy returns IDs from the oracle module
-	t.Run("test getting ids with two currency-pairs in module-state", func(t *testing.T) {
-		// expect the first currency-pair to have ID 0
-		ok.On("GetIDForCurrencyPair", ctx, btcusd).Return(uint64(0), true).Once()
-		id, err := strategy.ID(ctx, btcusd)
-		require.NoError(t, err)
-		require.Equal(t, uint64(0), id)
-
-		// expect the second currency-pair to have ID 1
-		ok.On("GetIDForCurrencyPair", ctx, usdeth).Return(uint64(1), true).Once()
-		id, err = strategy.ID(ctx, usdeth)
-		require.NoError(t, err)
-		require.Equal(t, uint64(1), id)
-	})
-
-	// test that if a currency-pair does not have an ID w/ x/oracle, a failure is returned
-	t.Run("expect error when currency-pair not found in module-state", func(t *testing.T) {
-		// expect an error when querying for a currency-pair not in module-state
-		ok.On("GetIDForCurrencyPair", ctx, ethbtc).Return(uint64(0), false).Once()
-		_, err := strategy.ID(ctx, ethbtc)
-		require.Error(t, err)
-	})
-}
-
-func TestDefaultCurrencyPairStrategyFromIDNoCache(t *testing.T) {
+func TestDefaultCurrencyPairStrategyFromID(t *testing.T) {
 	ok := mocks.NewOracleKeeper(t)
 
 	ctx := sdk.Context{}
 
 	strategy := strategies.NewDefaultCurrencyPairStrategy(ok)
-
-	// test that the strategy returns IDs from the oracle module
-	t.Run("test getting ids with two currency-pairs in module-state", func(t *testing.T) {
-		// expect the first currency-pair to have ID 0
-		ok.On("GetCurrencyPairFromID", ctx, uint64(0)).Return(btcusd, true).Once()
-		cp, err := strategy.FromID(ctx, uint64(0))
-		require.NoError(t, err)
-		require.Equal(t, btcusd, cp)
-
-		// expect the second currency-pair to have ID 1
-		ok.On("GetCurrencyPairFromID", ctx, uint64(1)).Return(usdeth, true).Once()
-		cp, err = strategy.FromID(ctx, uint64(1))
-		require.NoError(t, err)
-		require.Equal(t, usdeth, cp)
-	})
-
-	// test that if a currency-pair does not have an ID w/ x/oracle, a failure is returned
-	t.Run("expect error when currency-pair not found in module-state", func(t *testing.T) {
-		// expect an error when querying for a currency-pair not in module-state
-		ok.On("GetCurrencyPairFromID", ctx, uint64(2)).Return(slinkytypes.CurrencyPair{}, false).Once()
-		_, err := strategy.FromID(ctx, uint64(2))
-		require.Error(t, err)
-	})
-}
-
-func TestDefaultCurrencyPairStrategyFromIDWithCache(t *testing.T) {
-	ok := mocks.NewOracleKeeper(t)
-
-	ctx := sdk.Context{}
-
-	opts := strategies.WithCache()
-	strategy := strategies.NewDefaultCurrencyPairStrategy(ok, opts)
 
 	// test that the strategy returns IDs from the oracle module and then will come from the cache
 	t.Run("test getting ids with two currency-pairs in module-state", func(t *testing.T) {
