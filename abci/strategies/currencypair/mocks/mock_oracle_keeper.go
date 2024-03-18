06bab64edb5d042678259a05ee0a6a2beb7f3f50
aljo242 <alex@skip.money>
2024-03-18 19:52:37 -0400

    mock gen

--- abci/strategies/currencypair/mocks/mock_oracle_keeper.go
+++ abci/strategies/currencypair/mocks/mock_oracle_keeper.go
@@ -100,6 +100,34 @@ func (_m *OracleKeeper) GetPriceForCurrencyPair(ctx types.Context, cp pkgtypes.C
 	return r0, r1
 }
 
+// GetRemovedCPCounter provides a mock function with given fields: ctx
+func (_m *OracleKeeper) GetRemovedCPCounter(ctx types.Context) (uint64, error) {
+	ret := _m.Called(ctx)
+
+	if len(ret) == 0 {
+		panic("no return value specified for GetRemovedCPCounter")
+	}
+
+	var r0 uint64
+	var r1 error
+	if rf, ok := ret.Get(0).(func(types.Context) (uint64, error)); ok {
+		return rf(ctx)
+	}
+	if rf, ok := ret.Get(0).(func(types.Context) uint64); ok {
+		r0 = rf(ctx)
+	} else {
+		r0 = ret.Get(0).(uint64)
+	}
+
+	if rf, ok := ret.Get(1).(func(types.Context) error); ok {
+		r1 = rf(ctx)
+	} else {
+		r1 = ret.Error(1)
+	}
+
+	return r0, r1
+}
+
 // NewOracleKeeper creates a new instance of OracleKeeper. It also registers a testing interface on the mock and a cleanup function to assert the mocks expectations.
 // The first argument is typically a *testing.T value.
 func NewOracleKeeper(t interface {
