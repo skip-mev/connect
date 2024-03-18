62417e69e2deb6f4655eab158152d0d639013e50
aljo242 <alex@skip.money>
2024-03-18 18:05:13 -0400

    remove

--- x/oracle/keeper/msg_server.go
+++ x/oracle/keeper/msg_server.go
@@ -86,6 +86,8 @@ func (m *msgServer) RemoveCurrencyPairs(goCtx context.Context, req *types.MsgRem
 
 		// delete the currency pair from state
 		m.k.RemoveCurrencyPair(ctx, cp)
+
+		m.k.removedCPsCounter++
 	}
 
 	return nil, nil
