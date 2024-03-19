8bd823aa83b951c264f14050df67c5be668ceac7
aljo242 <alex@skip.money>
2024-03-18 20:14:24 -0400

    mocks

--- abci/strategies/currencypair/mocks/mock_oracle_keeper.go
+++ abci/strategies/currencypair/mocks/mock_oracle_keeper.go
@@ -16,6 +16,34 @@ type OracleKeeper struct {
 	mock.Mock
 }
 
+// GetCPCounter provides a mock function with given fields: ctx
+func (_m *OracleKeeper) GetCPCounter(ctx types.Context) (uint64, error) {
+	ret := _m.Called(ctx)
+
+	if len(ret) == 0 {
+		panic("no return value specified for GetCPCounter")
+	}
+
+	var r0 uint64
+	var r1 error
+	if rf, ok := ret.Get(0).(func(types.Context) (uint64, error)); ok {
+		return rf(ctx)
+	}
+	if rf, ok := ret.Get(0).(func(types.Context) uint64); ok {
+		r0 = rf(ctx)
+	} else {
+		r0 = ret.Get(0).(uint64)
+	}
+
+	if rf, ok := ret.Get(1).(func(types.Context) error); ok {
+		r1 = rf(ctx)
+	} else {
+		r1 = ret.Error(1)
+	}
+
+	return r0, r1
+}
+
 // GetCurrencyPairFromID provides a mock function with given fields: ctx, id
 func (_m *OracleKeeper) GetCurrencyPairFromID(ctx types.Context, id uint64) (pkgtypes.CurrencyPair, bool) {
 	ret := _m.Called(ctx, id)
