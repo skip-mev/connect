418fdfa5fa87d07b5298ed026f8a5fa942fa4950
aljo242 <alex@skip.money>
2024-03-20 00:43:24 -0400

    test

--- x/marketmap/types/msg.go
+++ x/marketmap/types/msg.go
@@ -63,5 +63,37 @@ func (m *MsgUpdateMarkets) ValidateBasic() error {
 		return err
 	}
 
+	for _, market := range m.UpdateMarkets {
+		if err := market.Ticker.ValidateBasic(); err != nil {
+			return err
+		}
+
+		for _, path := range market.Paths.Paths {
+			if err := path.ValidateBasic(); err != nil {
+				return err
+			}
+		}
+
+		if uint64(len(market.Providers.Providers)) < market.Ticker.MinProviderCount {
+			return fmt.Errorf("this ticker must have at least %d providers; got %d",
+				market.Ticker.MinProviderCount,
+				len(market.Providers.Providers),
+			)
+		}
+
+		seenProviders := make(map[string]struct{})
+		for _, provider := range market.Providers.Providers {
+			if err := provider.ValidateBasic(); err != nil {
+				return err
+			}
+
+			// check for duplicate providers
+			if _, seen := seenProviders[provider.Name]; seen {
+				return fmt.Errorf("duplicate provider found: %s", provider.Name)
+			}
+			seenProviders[provider.Name] = struct{}{}
+		}
+	}
+
 	return nil
 }
--- x/marketmap/types/msg_test.go
+++ x/marketmap/types/msg_test.go
@@ -243,3 +243,237 @@ func TestValidateBasicMsgCreateMarket(t *testing.T) {
 		})
 	}
 }
+
+func TestValidateBasicMsgUpdateMarket(t *testing.T) {
+	validCurrencyPair := slinkytypes.CurrencyPair{
+		Base:  "BTC",
+		Quote: "ETH",
+	}
+
+	validTicker := types.Ticker{
+		CurrencyPair:     validCurrencyPair,
+		Decimals:         8,
+		MinProviderCount: 2,
+	}
+
+	tcs := []struct {
+		name       string
+		msg        types.MsgUpdateMarkets
+		expectPass bool
+	}{
+		{
+			"if the signer is not an acc-address - fail",
+			types.MsgUpdateMarkets{
+				Signer: "invalid",
+			},
+			false,
+		},
+		{
+			"invalid ticker - fail",
+			types.MsgUpdateMarkets{
+				Signer: sample.Address(sample.Rand()),
+				UpdateMarkets: []types.UpdateMarket{
+					{
+						Ticker: types.Ticker{
+							CurrencyPair:     validCurrencyPair,
+							Decimals:         0,
+							MinProviderCount: 0,
+						},
+						Providers: types.Providers{
+							Providers: []types.ProviderConfig{
+								{
+									Name:           "kucoin",
+									OffChainTicker: "btc-eth",
+								},
+								{
+									Name:           "mexc",
+									OffChainTicker: "btceth",
+								},
+							},
+						},
+						Paths: types.Paths{
+							Paths: []types.Path{
+								{
+									Operations: []types.Operation{
+										{
+											CurrencyPair: validTicker.CurrencyPair,
+											Invert:       false,
+										},
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+			false,
+		},
+		{
+			"invalid num providers - fail",
+			types.MsgUpdateMarkets{
+				Signer: sample.Address(sample.Rand()),
+				UpdateMarkets: []types.UpdateMarket{
+					{
+						Ticker: validTicker,
+						Providers: types.Providers{
+							Providers: []types.ProviderConfig{
+								{
+									Name:           "kucoin",
+									OffChainTicker: "btc-eth",
+								},
+							},
+						},
+						Paths: types.Paths{
+							Paths: []types.Path{
+								{
+									Operations: []types.Operation{
+										{
+											CurrencyPair: validTicker.CurrencyPair,
+											Invert:       false,
+										},
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+			false,
+		},
+		{
+			"invalid empty offchain ticker - fail",
+			types.MsgUpdateMarkets{
+				Signer: sample.Address(sample.Rand()),
+				UpdateMarkets: []types.UpdateMarket{
+					{
+						Ticker: validTicker,
+						Providers: types.Providers{
+							Providers: []types.ProviderConfig{
+								{
+									Name:           "kucoin",
+									OffChainTicker: "btc-eth",
+								},
+								{
+									Name:           "mexc",
+									OffChainTicker: "",
+								},
+							},
+						},
+						Paths: types.Paths{
+							Paths: []types.Path{
+								{
+									Operations: []types.Operation{
+										{
+											CurrencyPair: validTicker.CurrencyPair,
+											Invert:       false,
+										},
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+			false,
+		},
+		{
+			"invalid no paths - fail",
+			types.MsgUpdateMarkets{
+				Signer: sample.Address(sample.Rand()),
+				UpdateMarkets: []types.UpdateMarket{
+					{
+						Ticker: validTicker,
+						Providers: types.Providers{
+							Providers: []types.ProviderConfig{
+								{
+									Name:           "kucoin",
+									OffChainTicker: "btc-eth",
+								},
+								{
+									Name:           "mexc",
+									OffChainTicker: "",
+								},
+							},
+						},
+						Paths: types.Paths{},
+					},
+				},
+			},
+			false,
+		},
+		{
+			"invalid path - fail",
+			types.MsgUpdateMarkets{
+				Signer: sample.Address(sample.Rand()),
+				UpdateMarkets: []types.UpdateMarket{
+					{
+						Ticker: validTicker,
+						Providers: types.Providers{
+							Providers: []types.ProviderConfig{
+								{
+									Name:           "kucoin",
+									OffChainTicker: "btc-eth",
+								},
+								{
+									Name:           "mexc",
+									OffChainTicker: "",
+								},
+							},
+						},
+						Paths: types.Paths{
+							Paths: make([]types.Path, 0),
+						},
+					},
+				},
+			},
+			false,
+		},
+		{
+			"valid message",
+			types.MsgUpdateMarkets{
+				Signer: sample.Address(sample.Rand()),
+				UpdateMarkets: []types.UpdateMarket{
+					{
+						Ticker: validTicker,
+						Providers: types.Providers{
+							Providers: []types.ProviderConfig{
+								{
+									Name:           "kucoin",
+									OffChainTicker: "btc-eth",
+								},
+								{
+									Name:           "mexc",
+									OffChainTicker: "btceth",
+								},
+							},
+						},
+						Paths: types.Paths{
+							Paths: []types.Path{
+								{
+									Operations: []types.Operation{
+										{
+											CurrencyPair: validTicker.CurrencyPair,
+											Invert:       false,
+										},
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+			true,
+		},
+	}
+
+	for _, tc := range tcs {
+		t.Run(tc.name, func(t *testing.T) {
+			err := tc.msg.ValidateBasic()
+			if !tc.expectPass {
+				require.NotNil(t, err)
+			} else {
+				require.Nil(t, err)
+			}
+		})
+	}
+}
