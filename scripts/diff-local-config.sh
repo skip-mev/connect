ae680733d129294cafc6448e32f3d1912f75a47d
Eric <eric.warehime@gmail.com>
2024-03-19 20:02:13 -0700

    Add the script

---
+++ scripts/diff-local-config.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Usage: make diff-local-config
+
+echo "Diffing local configs against HEAD"
+diff_output=$(git diff HEAD ./config/local/*.json)
+
+if [ -z "$diff_output" ]; then
+    echo "Local configs are up to date."
+else
+    echo "Local configs are not updated. Run make update-local-config to update."
+    echo "$diff_output"
+    exit 1
+fi
