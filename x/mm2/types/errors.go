d67358d80671077868f7d85ffab6655ddab89918
aljo242 <alex@skip.money>
2024-03-22 16:49:48 -0400

    errors

---
+++ x/mm2/types/errors.go
@@ -0,0 +1,31 @@
+package types
+
+import "fmt"
+
+// MarketAlreadyExistsError is an error indicating the given Market exists in state.
+type MarketAlreadyExistsError struct {
+	ticker TickerString
+}
+
+func NewMarketAlreadyExistsError(ticker TickerString) MarketAlreadyExistsError {
+	return MarketAlreadyExistsError{ticker: ticker}
+}
+
+// Error returns the error string for MarketAlreadyExistsError.
+func (e MarketAlreadyExistsError) Error() string {
+	return fmt.Sprintf("market already exists for ticker %s", e.ticker)
+}
+
+// MarketDoesNotExistsError is an error indicating the given Market does not exist in state.
+type MarketDoesNotExistsError struct {
+	ticker TickerString
+}
+
+func NewMarketDoesNotExistsError(ticker TickerString) MarketDoesNotExistsError {
+	return MarketDoesNotExistsError{ticker: ticker}
+}
+
+// Error returns the error string for MarketDoesNotExistsError.
+func (e MarketDoesNotExistsError) Error() string {
+	return fmt.Sprintf("market does not exist for ticker %s", e.ticker)
+}
