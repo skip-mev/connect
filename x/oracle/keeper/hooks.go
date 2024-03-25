81125502689b879dfc94ef726af88aa81a021111
aljo242 <alex@skip.money>
2024-03-25 18:17:42 -0400

    this is my legacy

--- x/oracle/keeper/hooks.go
+++ x/oracle/keeper/hooks.go
@@ -24,22 +24,22 @@ func (k *Keeper) Hooks() Hooks {
 	return Hooks{k}
 }
 
-// AfterMarketCreated is the marketmap hook for x/oracle that is run after a market is created in
+// LegacyAfterMarketCreated is the marketmap hook for x/oracle that is run after a market is created in
 // the marketmap.  After the market is created, a currency pair and its state are initialized in the
 // oracle module.
-func (h Hooks) AfterMarketCreated(ctx sdk.Context, ticker marketmaptypes.Ticker) error {
+func (h Hooks) LegacyAfterMarketCreated(ctx sdk.Context, ticker marketmaptypes.Ticker) error {
 	return h.k.CreateCurrencyPair(ctx, ticker.CurrencyPair)
 }
 
-func (h Hooks) AfterMarketUpdated(_ sdk.Context, _ marketmaptypes.Ticker) error {
+func (h Hooks) LegacyAfterMarketUpdated(_ sdk.Context, _ marketmaptypes.Ticker) error {
 	// TODO finish
 
 	return nil
 }
 
-// AfterMarketGenesis verifies that all tickers set in the x/marketmap genesis are registered in
+// LegacyAfterMarketGenesis verifies that all tickers set in the x/marketmap genesis are registered in
 // the x/oracle module.
-func (h Hooks) AfterMarketGenesis(ctx sdk.Context, tickers map[string]marketmaptypes.Ticker) error {
+func (h Hooks) LegacyAfterMarketGenesis(ctx sdk.Context, tickers map[string]marketmaptypes.Ticker) error {
 	for _, ticker := range tickers {
 		if !h.k.HasCurrencyPair(ctx, ticker.CurrencyPair) {
 			return fmt.Errorf("currency pair %s is registered in x/marketmap but not in x/oracle", ticker.String())
@@ -49,23 +49,23 @@ func (h Hooks) AfterMarketGenesis(ctx sdk.Context, tickers map[string]marketmapt
 	return nil
 }
 
-// AfterMarketCreated_ is the marketmap hook for x/oracle that is run after a market is created in
+// AfterMarketCreated is the marketmap hook for x/oracle that is run after a market is created in
 // the marketmap.  After the market is created, a currency pair and its state are initialized in the
-// oracle module.v
-func (h Hooks) AfterMarketCreated_(ctx sdk.Context, market mm2types.Market) error {
+// oracle module.
+func (h Hooks) AfterMarketCreated(ctx sdk.Context, market mm2types.Market) error {
 	return h.k.CreateCurrencyPair(ctx, market.Ticker.CurrencyPair)
 }
 
-// AfterMarketUpdated_ is the marketmap hook for x/oracle that is run after a market is updated in
+// AfterMarketUpdated is the marketmap hook for x/oracle that is run after a market is updated in
 // the marketmap.
-func (h Hooks) AfterMarketUpdated_(ctx sdk.Context, market mm2types.Market) error {
+func (h Hooks) AfterMarketUpdated(ctx sdk.Context, market mm2types.Market) error {
 	// TODO
 	return nil
 }
 
-// AfterMarketGenesis_ verifies that all markets set in the x/marketmap genesis are registered in
+// AfterMarketGenesis verifies that all markets set in the x/marketmap genesis are registered in
 // the x/oracle module.
-func (h Hooks) AfterMarketGenesis_(ctx sdk.Context, markets map[string]mm2types.Market) error {
+func (h Hooks) AfterMarketGenesis(ctx sdk.Context, markets map[string]mm2types.Market) error {
 	for _, market := range markets {
 		if !h.k.HasCurrencyPair(ctx, market.Ticker.CurrencyPair) {
 			return fmt.Errorf("currency pair %s is registered in x/marketmap but not in x/oracle", market.Ticker.String())
