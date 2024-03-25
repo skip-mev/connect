2d82de7d451e6c6f1d198b56ee8b619169297cdb
aljo242 <alex@skip.money>
2024-03-25 09:53:57 -0400

    msg :

--- x/mm2/types/msg.go
+++ x/mm2/types/msg.go
@@ -1,1 +1,56 @@
 package types
+
+import (
+	sdk "github.com/cosmos/cosmos-sdk/types"
+)
+
+var (
+	_ sdk.Msg = &MsgCreateMarkets{}
+	_ sdk.Msg = &MsgUpdateMarkets{}
+	_ sdk.Msg = &MsgParams{}
+)
+
+// ValidateBasic determines whether the information in the message is formatted correctly, specifically
+// whether the signer is a valid acc-address.
+func (m *MsgCreateMarkets) ValidateBasic() error {
+	// validate signer address
+	if _, err := sdk.AccAddressFromBech32(m.Authority); err != nil {
+		return err
+	}
+
+	for _, market := range m.CreateMarkets {
+		if err := market.ValidateBasic(); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+// ValidateBasic determines whether the information in the message is formatted correctly, specifically
+// whether the signer is a valid acc-address.
+func (m *MsgUpdateMarkets) ValidateBasic() error {
+	// validate signer address
+	if _, err := sdk.AccAddressFromBech32(m.Authority); err != nil {
+		return err
+	}
+
+	for _, market := range m.UpdateMarkets {
+		if err := market.ValidateBasic(); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+// ValidateBasic determines whether the information in the message is formatted correctly, specifically
+// whether the signer is a valid acc-address.
+func (m *MsgParams) ValidateBasic() error {
+	// validate signer address
+	if _, err := sdk.AccAddressFromBech32(m.Authority); err != nil {
+		return err
+	}
+
+	return m.Params.ValidateBasic()
+}
