f4157fc1336e715076f9d61592671963aac557d7
David Terpay <david.terpay@gmail.com>
2024-04-01 11:32:31 -0400

    keeper fixes

--- x/oracle/keeper/abci_test.go
+++ x/oracle/keeper/abci_test.go
@@ -7,7 +7,7 @@ import (
 func (s *KeeperTestSuite) TestBeginBlocker() {
 	s.Run("run with no state", func() {
 		s.Require().NoError(s.oracleKeeper.BeginBlocker(s.ctx))
-		removes, err := s.oracleKeeper.GetRemovedCPCounter(s.ctx)
+		removes, err := s.oracleKeeper.GetNumRemovedCurrencyPairs(s.ctx)
 		s.Require().NoError(err)
 		s.Require().Equal(removes, uint64(0))
 	})
@@ -17,11 +17,11 @@ func (s *KeeperTestSuite) TestBeginBlocker() {
 		s.Require().NoError(s.oracleKeeper.RemoveCurrencyPair(s.ctx, slinkytypes.CurrencyPair{Base: "test", Quote: "coin1"}))
 
 		s.Require().NoError(s.oracleKeeper.BeginBlocker(s.ctx))
-		removes, err := s.oracleKeeper.GetRemovedCPCounter(s.ctx)
+		removes, err := s.oracleKeeper.GetNumRemovedCurrencyPairs(s.ctx)
 		s.Require().NoError(err)
 		s.Require().Equal(removes, uint64(0))
 
-		cps, err := s.oracleKeeper.GetPrevBlockCPCounter(s.ctx)
+		cps, err := s.oracleKeeper.GetNumCurrencyPairs(s.ctx)
 		s.Require().NoError(err)
 		s.Require().Equal(cps, uint64(0))
 	})
@@ -32,11 +32,11 @@ func (s *KeeperTestSuite) TestBeginBlocker() {
 		s.Require().NoError(s.oracleKeeper.CreateCurrencyPair(s.ctx, slinkytypes.CurrencyPair{Base: "test", Quote: "coin2"}))
 
 		s.Require().NoError(s.oracleKeeper.BeginBlocker(s.ctx))
-		removes, err := s.oracleKeeper.GetRemovedCPCounter(s.ctx)
+		removes, err := s.oracleKeeper.GetNumRemovedCurrencyPairs(s.ctx)
 		s.Require().NoError(err)
 		s.Require().Equal(removes, uint64(0))
 
-		cps, err := s.oracleKeeper.GetPrevBlockCPCounter(s.ctx)
+		cps, err := s.oracleKeeper.GetNumCurrencyPairs(s.ctx)
 		s.Require().NoError(err)
 		s.Require().Equal(cps, uint64(1))
 	})
