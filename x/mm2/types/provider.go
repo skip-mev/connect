6540106330201625dc7bf7ea83550afc978012df
Alex Johnson <alex@skip.money>
2024-04-02 12:23:36 -0400

    feat(`x/marketmap`): init types and validation (#266)
    * clean
    
    * new market
    
    * gen
    
    * fix
    
    * wip
    
    * dep
    
    * update
    
    * cute
    
    * clean
    
    * len
    
    * cp
    
    * ok
    
    * nice
    
    * Update x/mm2/types/market.go
    
    Co-authored-by: David Terpay <35130517+davidterpay@users.noreply.github.com>
    
    * Update x/mm2/types/market.go
    
    Co-authored-by: David Terpay <35130517+davidterpay@users.noreply.github.com>
    
    * fix
    
    * test
    
    * comment
    
    * expand tests
    
    * better errors
    
    ---------
    
    Co-authored-by: David Terpay <35130517+davidterpay@users.noreply.github.com>

---
+++ x/mm2/types/provider.go
@@ -0,0 +1,66 @@
+package types
+
+import (
+	"fmt"
+
+	"github.com/skip-mev/slinky/pkg/json"
+)
+
+// ValidateBasic performs basic validation on a ProviderConfig.
+func (pc *ProviderConfig) ValidateBasic() error {
+	if len(pc.Name) == 0 {
+		return fmt.Errorf("provider name must not be empty")
+	}
+
+	if len(pc.OffChainTicker) == 0 {
+		return fmt.Errorf("provider offchain ticker must not be empty")
+	}
+
+	// NormalizeByPair is allowed to be empty
+	if pc.NormalizeByPair != nil {
+		if err := pc.NormalizeByPair.ValidateBasic(); err != nil {
+			return err
+		}
+	}
+
+	if len(pc.Metadata_JSON) > MaxMetadataJSONFieldLength {
+		return fmt.Errorf("metadata json field is longer than maximum length of %d", MaxMetadataJSONFieldLength)
+	}
+
+	if err := json.IsValid([]byte(pc.Metadata_JSON)); err != nil {
+		return fmt.Errorf("invalid provider config metadata json: %w", err)
+	}
+
+	return nil
+}
+
+// Equal returns true iff the ProviderConfig is equal to the given ProviderConfig.
+func (pc *ProviderConfig) Equal(other ProviderConfig) bool {
+	if pc.Name != other.Name {
+		return false
+	}
+
+	if pc.OffChainTicker != other.OffChainTicker {
+		return false
+	}
+
+	if pc.Invert != other.Invert {
+		return false
+	}
+
+	if pc.NormalizeByPair == nil {
+		if other.NormalizeByPair != nil {
+			return false
+		}
+	} else {
+		if other.NormalizeByPair == nil {
+			return false
+		}
+
+		if !pc.NormalizeByPair.Equal(*other.NormalizeByPair) {
+			return false
+		}
+	}
+
+	return pc.Metadata_JSON == other.Metadata_JSON
+}
---
+++ x/mm2/types/ticker.go
@@ -0,0 +1,72 @@
+package types
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/skip-mev/slinky/pkg/json"
+
+	slinkytypes "github.com/skip-mev/slinky/pkg/types"
+)
+
+const (
+	// DefaultMaxDecimals is the maximum number of decimals allowed for a ticker.
+	DefaultMaxDecimals = 36
+	// DefaultMinProviderCount is the minimum number of providers required for a
+	// ticker to be considered valid.
+	DefaultMinProviderCount = 1
+	// MaxMetadataJSONFieldLength is the maximum length of the MetadataJSON field.
+	MaxMetadataJSONFieldLength = 16384
+)
+
+// NewTicker returns a new Ticker instance. A Ticker represents a price feed for
+// a given asset pair i.e. BTC/USD. The price feed is scaled to a number of decimal
+// places and has a minimum number of providers required to consider the ticker valid.
+func NewTicker(base, quote string, decimals, minProviderCount uint64) Ticker {
+	return Ticker{
+		CurrencyPair: slinkytypes.CurrencyPair{
+			Base:  strings.ToUpper(base),
+			Quote: strings.ToUpper(quote),
+		},
+		Decimals:         decimals,
+		MinProviderCount: minProviderCount,
+	}
+}
+
+// String returns a string representation of the Ticker.
+func (t Ticker) String() string {
+	return t.CurrencyPair.String()
+}
+
+// ValidateBasic performs basic validation on the Ticker.
+func (t *Ticker) ValidateBasic() error {
+	if t.Decimals > DefaultMaxDecimals || t.Decimals == 0 {
+		return fmt.Errorf("decimals must be between 1 and %d; got %d for %s", DefaultMaxDecimals, t.Decimals, t.CurrencyPair.String())
+	}
+	if t.MinProviderCount < DefaultMinProviderCount {
+		return fmt.Errorf("min provider count must be at least %d; got %d for %s", DefaultMinProviderCount, t.MinProviderCount, t.CurrencyPair.String())
+	}
+
+	if err := t.CurrencyPair.ValidateBasic(); err != nil {
+		return err
+	}
+
+	if len(t.Metadata_JSON) > MaxMetadataJSONFieldLength {
+		return fmt.Errorf("metadata json field is longer than maximum length of %d", MaxMetadataJSONFieldLength)
+	}
+
+	if err := json.IsValid([]byte(t.Metadata_JSON)); err != nil {
+		return fmt.Errorf("invalid ticker metadata json: %w", err)
+	}
+
+	return nil
+}
+
+// Equal returns true iff the Ticker is equal to the given Ticker.
+func (t *Ticker) Equal(other Ticker) bool {
+	return t.CurrencyPair.Equal(other.CurrencyPair) &&
+		t.Decimals == other.Decimals &&
+		t.MinProviderCount == other.MinProviderCount &&
+		t.Metadata_JSON == other.Metadata_JSON &&
+		t.Enabled == other.Enabled
+}
