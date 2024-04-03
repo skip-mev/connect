95746e979ee05b8058cd79bf344c4287e5c5b325
Alex Johnson <alex@skip.money>
2024-04-03 19:15:54 -0400

    feat(`x/marketmap`):  genesis and validation (#268)

---
+++ x/mm2/types/keys.go
@@ -0,0 +1,34 @@
+package types
+
+import (
+	"cosmossdk.io/collections"
+	"cosmossdk.io/collections/codec"
+)
+
+const (
+	// ModuleName defines the canonical name identifying the module.
+	ModuleName = "marketmap"
+	// StoreKey holds the unique key used to access the module keeper's KVStore.
+	StoreKey = ModuleName
+)
+
+var (
+	// LastUpdatedPrefix is the key prefix for the lastUpdated height.
+	LastUpdatedPrefix = collections.NewPrefix(1)
+
+	// MarketsPrefix is the key prefix for Markets.
+	MarketsPrefix = collections.NewPrefix(2)
+
+	// ParamsPrefix is the key prefix of the module Params.
+	ParamsPrefix = collections.NewPrefix(3)
+
+	// TickersCodec is the collections.KeyCodec value used for the markets map.
+	TickersCodec = codec.NewStringKeyCodec[TickerString]()
+
+	// LastUpdatedCodec is the collections.KeyCodec value used for the lastUpdated value.
+	LastUpdatedCodec = codec.KeyToValueCodec[uint64](codec.NewUint64Key[uint64]())
+)
+
+// TickerString is the key used to identify unique pairs of Base/Quote with corresponding PathsConfig objects--or in other words AggregationConfigs.
+// The TickerString is identical to Slinky's CurrencyPair.String() output in that it is `Base` and `Quote` joined by `/` i.e. `$BASE/$QUOTE`.
+type TickerString string
