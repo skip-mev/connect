053211cb0171756927bc2798363f3e628d72f26b
Alex Johnson <alex@skip.money>
2024-04-03 17:28:37 -0400

    feat(`x/marketmap`): params type (#267)
    * clean
    
    * new market
    
    * gen
    
    * fix
    
    * wip
    
    * dep
    
    * update
    
    * cute
    
    * clean
    
    * params
    
    * fix
    
    * len
    
    * cp
    
    * ok
    
    * nice
    
    * Update x/mm2/types/market.go
    
    Co-authored-by: David Terpay <35130517+davidterpay@users.noreply.github.com>
    
    * Update x/mm2/types/market.go
    
    Co-authored-by: David Terpay <35130517+davidterpay@users.noreply.github.com>
    
    * fix
    
    * test
    
    * newline
    
    * remove
    
    * comment
    
    * expand tests
    
    * fix
    
    ---------
    
    Co-authored-by: David Terpay <35130517+davidterpay@users.noreply.github.com>

---
+++ x/mm2/types/params_test.go
@@ -0,0 +1,71 @@
+package types_test
+
+import (
+	"testing"
+
+	authtypes "github.com/cosmos/cosmos-sdk/x/auth/types"
+	"github.com/stretchr/testify/require"
+
+	"github.com/skip-mev/slinky/x/mm2/types"
+)
+
+func TestValidateBasic(t *testing.T) {
+	type testCase struct {
+		name      string
+		params    types.Params
+		expectErr bool
+	}
+
+	testCases := []testCase{
+		{
+			name:      "valid default params",
+			params:    types.DefaultParams(),
+			expectErr: false,
+		},
+		{
+			name: "valid multiple authorities",
+			params: types.Params{
+				MarketAuthorities: []string{authtypes.NewModuleAddress(authtypes.ModuleName).String(), types.DefaultMarketAuthority},
+			},
+			expectErr: false,
+		},
+		{
+			name: "invalid duplicate authority",
+			params: types.Params{
+				MarketAuthorities: []string{types.DefaultMarketAuthority, types.DefaultMarketAuthority},
+			},
+			expectErr: true,
+		},
+		{
+			name: "invalid authority string",
+			params: types.Params{
+				MarketAuthorities: []string{"incorrect"},
+			},
+			expectErr: true,
+		},
+		{
+			name: "invalid nil authority",
+			params: types.Params{
+				MarketAuthorities: nil,
+			},
+			expectErr: true,
+		},
+		{
+			name:      "invalid empty params",
+			params:    types.Params{},
+			expectErr: true,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			err := tc.params.ValidateBasic()
+			if tc.expectErr {
+				require.Error(t, err)
+				return
+			}
+
+			require.NoError(t, err)
+		})
+	}
+}
