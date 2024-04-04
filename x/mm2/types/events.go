9b4975e5d6144c631079c62e4caa9ff3a2043067
Alex Johnson <alex@skip.money>
2024-04-04 12:57:01 -0400

    feat(`x/marketmap`): events (#281)
    * types
    
    * events
    
    ---------
    
    Co-authored-by: David Terpay <35130517+davidterpay@users.noreply.github.com>

---
+++ x/mm2/types/events.go
@@ -0,0 +1,13 @@
+package types
+
+// market map module event types
+
+const (
+	EventTypeCreateMarket = "create_market"
+	EventTypeUpdateMarket = "update_market"
+
+	AttributeKeyCurrencyPair     = "currency_pair"
+	AttributeKeyDecimals         = "decimals"
+	AttributeKeyMinProviderCount = "min_provider_count"
+	AttributeKeyMetadata         = "metadata"
+)
