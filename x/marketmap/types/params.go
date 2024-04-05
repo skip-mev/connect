68f6d54501fd975e873d35c04ca9dab7437f9456
beer-1 <147697694+beer-1@users.noreply.github.com>
2024-04-05 16:05:25 +0900

    compute gov addr after config changed

--- x/marketmap/types/params.go
+++ x/marketmap/types/params.go
@@ -13,13 +13,10 @@ const (
 	DefaultVersion = 0
 )
 
-// DefaultMarketAuthority is the default value for the market authority Param.
-var DefaultMarketAuthority = authtypes.NewModuleAddress(govtypes.ModuleName).String()
-
 // DefaultParams returns default marketmap parameters.
 func DefaultParams() Params {
 	return Params{
-		MarketAuthority: DefaultMarketAuthority,
+		MarketAuthority: authtypes.NewModuleAddress(govtypes.ModuleName).String(),
 		Version:         DefaultVersion,
 	}
 }
