eaac82a4fa0e9e48f75d29c78aa62262fbfa070e
Eric Warehime <eric.warehime@gmail.com>
2024-04-15 09:25:15 -0700

    feat: Compatibility between the voteweighted package and ICS consumer keeper (#315)

--- pkg/math/voteweighted/interfaces.go
+++ pkg/math/voteweighted/interfaces.go
@@ -6,6 +6,7 @@ import (
 	"cosmossdk.io/math"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	stakingtypes "github.com/cosmos/cosmos-sdk/x/staking/types"
+	"github.com/cosmos/interchain-security/v5/x/ccv/consumer/types"
 )
 
 // ValidatorStore defines the interface contract required for calculating stake-weighted median
@@ -16,4 +17,12 @@ import (
 type ValidatorStore interface {
 	ValidatorByConsAddr(ctx context.Context, addr sdk.ConsAddress) (stakingtypes.ValidatorI, error)
 	TotalBondedTokens(ctx context.Context) (math.Int, error)
+}
+
+// CCValidatorStore defines the interface contract required for the cross chain validator consumer store.
+//
+//go:generate mockery --name CCValidatorStore --filename mock_cc_validator_store.go
+type CCValidatorStore interface {
+	GetAllCCValidator(ctx sdk.Context) []types.CrossChainValidator
+	GetCCValidator(ctx sdk.Context, addr []byte) (types.CrossChainValidator, bool)
 }
