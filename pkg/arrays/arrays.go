206abb595db71993710376e1edc844a1f1f54350
Nikhil Vasan <97126437+nivasan1@users.noreply.github.com>
2024-05-03 13:31:39 -0700

    Update pkg/arrays/arrays.go
    Co-authored-by: Alex Johnson <alex@skip.money>

--- pkg/arrays/arrays.go
+++ pkg/arrays/arrays.go
@@ -1,6 +1,6 @@
 package arrays
 
-// CheckEntryInArray checks if an entry is in an array, and returns a boolean value
+// CheckEntryInArray checks if an entry is in an array, and returns true
 // and the entry if it is found.
 func CheckEntryInArray[T comparable](entry T, array []T) (value T, _ bool) {
 	for _, e := range array {
