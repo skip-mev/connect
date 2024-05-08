c2dd570ed28269a8c4e481172831e493ab96b131
David Terpay <david.terpay@gmail.com>
2024-05-08 10:43:53 -0400

    init

--- abci/strategies/currencypair/hash.go
+++ abci/strategies/currencypair/hash.go
@@ -9,9 +9,9 @@ import (
 	slinkytypes "github.com/skip-mev/slinky/pkg/types"
 )
 
-// HashCurrencyPairStrategy is a strategy that uses the hash of the currency pair as the unique ID
-// for a given currency pair and utilizes raw prices stored in the x/oracle state as the price
-// representation for a given currency pair.
+// HashCurrencyPairStrategy is a strategy that uses the sha256 hash of the currency
+// pair as the unique ID for a given currency pair and utilizes raw prices as the
+// price representation for a given currency pair.
 type HashCurrencyPairStrategy struct {
 	DefaultCurrencyPairStrategy
 }
@@ -23,8 +23,8 @@ func NewHashCurrencyPairStrategy(oracleKeeper OracleKeeper) *HashCurrencyPairStr
 	}
 }
 
-// ID returns the ID of the given currency pair, by taking the hash of the currency pair and using
-// that as the ID.
+// ID returns the ID of the given currency pair, by taking the hash of the currency
+// pair and using that as the ID.
 func (s *HashCurrencyPairStrategy) ID(ctx sdk.Context, cp slinkytypes.CurrencyPair) (uint64, error) {
 	// reset cache if the block height has changed
 	height := ctx.BlockHeight()
@@ -48,9 +48,9 @@ func (s *HashCurrencyPairStrategy) ID(ctx sdk.Context, cp slinkytypes.CurrencyPa
 	return hash, nil
 }
 
-// FromID returns the currency pair with the given ID, it first checks the cache for the currency pair
-// and if it is not found in the cache, it will attempt to retrieve the currency pair from the x/oracle
-// state.
+// FromID returns the currency pair with the given ID, it first checks the cache
+// for the currency pair and if it is not found in the cache, it will attempt to
+// retrieve the currency pair from the x/oracle state.
 func (s *HashCurrencyPairStrategy) FromID(ctx sdk.Context, id uint64) (slinkytypes.CurrencyPair, error) {
 	// reset cache if the block height has changed
 	height := ctx.BlockHeight()
@@ -64,8 +64,9 @@ func (s *HashCurrencyPairStrategy) FromID(ctx sdk.Context, id uint64) (slinkytyp
 		return cp, nil
 	}
 
-	// if the currency pair is not found in the cache, attempt to retrieve it from the x/oracle state by
-	// populating the cache with all currency pairs.
+	// if the currency pair is not found in the cache, attempt to retrieve it from
+	// the x/oracle state by populating the cache with all currency pairs. This
+	// should only be executed once per block height.
 	allCPs := s.oracleKeeper.GetAllCurrencyPairs(ctx)
 	for _, cp := range allCPs {
 		hash, err := CurrencyPairToHashID(cp.String())
@@ -84,8 +85,8 @@ func (s *HashCurrencyPairStrategy) FromID(ctx sdk.Context, id uint64) (slinkytyp
 	return cp, nil
 }
 
-// CurrencyPairToHashID returns the ID of the given currency pair, by taking the sha256 hash of the
-// currency pair.
+// CurrencyPairToHashID returns the ID of the given currency pair, by taking the
+// sha256 hash of the currency pair.
 func CurrencyPairToHashID(currencyPair string) (uint64, error) {
 	hash := sha256.New()
 	if _, err := hash.Write([]byte(currencyPair)); err != nil {
