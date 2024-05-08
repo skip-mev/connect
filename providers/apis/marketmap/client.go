f10b55b105213223cacc241c03e480dad9a0be01
David Terpay <david.terpay@gmail.com>
2024-05-08 13:40:50 -0400

    nit

--- providers/apis/marketmap/client.go
+++ providers/apis/marketmap/client.go
@@ -5,26 +5,18 @@ import (
 	mmtypes "github.com/skip-mev/slinky/x/marketmap/types"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
-	"google.golang.org/grpc/keepalive"
 )
 
 // NewGRPCClient returns a new GRPC client for MarketMap module.
 func NewGRPCClient(
 	config config.APIConfig,
 ) (mmtypes.QueryClient, error) {
-	kacp := keepalive.ClientParameters{
-		Time:                config.Interval, // send pings every 10 seconds if there is no activity
-		Timeout:             config.Timeout,  // wait a second for ping ack before considering the connection dead
-		PermitWithoutStream: true,            // send pings even without active streams
-	}
-
 	// TODO: Do we want to ignore proxy settings?
 	conn, err := grpc.Dial(
 		config.URL,
 		grpc.WithTransportCredentials(insecure.NewCredentials()),
 		grpc.WithDefaultCallOptions(grpc.MaxCallRecvMsgSize(1024*1024*4)), // Set max receive message size to 4MB
 		grpc.WithDefaultCallOptions(grpc.MaxCallSendMsgSize(1024*1024*4)), // Set max send message size to 4MB
-		grpc.WithKeepaliveParams(kacp),
 	)
 	if err != nil {
 		return nil, err
