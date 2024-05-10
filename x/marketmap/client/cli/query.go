355c3d416415bb40b6d4269a4ce4a7df63da19df
David Terpay <david.terpay@gmail.com>
2024-05-10 02:43:59 -0400

    init

--- x/marketmap/client/cli/query.go
+++ x/marketmap/client/cli/query.go
@@ -42,7 +42,7 @@ func CmdQueryParams() *cobra.Command {
 			}
 
 			queryClient := types.NewQueryClient(clientCtx)
-			res, err := queryClient.Params(clientCtx.CmdContext, &types.ParamsRequest{})
+			res, err := queryClient.Params(cmd.Context(), &types.ParamsRequest{})
 			if err != nil {
 				return err
 			}
@@ -67,7 +67,7 @@ func CmdQueryMarketMap() *cobra.Command {
 			}
 
 			queryClient := types.NewQueryClient(clientCtx)
-			res, err := queryClient.MarketMap(clientCtx.CmdContext, &types.MarketMapRequest{})
+			res, err := queryClient.MarketMap(cmd.Context(), &types.MarketMapRequest{})
 			if err != nil {
 				return err
 			}
@@ -82,9 +82,10 @@ func CmdQueryMarketMap() *cobra.Command {
 
 func CmdQueryMarket() *cobra.Command {
 	cmd := &cobra.Command{
-		Use:   "market",
-		Short: "Query the a market using the given currency pair",
-		Args:  cobra.ExactArgs(1),
+		Use:     "market",
+		Short:   "Query the a market using the given currency pair",
+		Example: "slinky marketmap market btc/usd",
+		Args:    cobra.ExactArgs(1),
 		RunE: func(cmd *cobra.Command, args []string) error {
 			clientCtx, err := client.GetClientQueryContext(cmd)
 			if err != nil {
@@ -97,7 +98,7 @@ func CmdQueryMarket() *cobra.Command {
 			}
 
 			queryClient := types.NewQueryClient(clientCtx)
-			res, err := queryClient.Market(clientCtx.CmdContext, &types.MarketRequest{
+			res, err := queryClient.Market(cmd.Context(), &types.MarketRequest{
 				CurrencyPair: cp,
 			})
 			if err != nil {
@@ -124,7 +125,7 @@ func CmdQueryLastUpdated() *cobra.Command {
 			}
 
 			queryClient := types.NewQueryClient(clientCtx)
-			res, err := queryClient.LastUpdated(clientCtx.CmdContext, &types.LastUpdatedRequest{})
+			res, err := queryClient.LastUpdated(cmd.Context(), &types.LastUpdatedRequest{})
 			if err != nil {
 				return err
 			}
