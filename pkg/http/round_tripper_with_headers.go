e4685fbc1f722c6346e32060cddbae70ef65a8d2
Nikhil Vasan <nikhil@skip.money>
2024-05-22 10:28:00 -0700

    add user-agent header to outbound requests to solana ndoes

--- pkg/http/round_tripper_with_headers.go
+++ pkg/http/round_tripper_with_headers.go
@@ -2,7 +2,29 @@ package http
 
 import (
 	"net/http"
+
+	"github.com/skip-mev/slinky/cmd/build"
 )
+
+const (
+	// UserAgentHeaderKey is the key for the User-Agent header.
+	UserAgentHeaderKey = "User-Agent"
+)
+
+// HeaderOption is a function that sets a (key, value) pair in the headers map.
+type HeaderOption func(map[string]string)
+
+func WithSlinkyVersionUserAgent() HeaderOption {
+	return func(header map[string]string) {
+		header[UserAgentHeaderKey] = build.Build
+	}
+}
+
+func WithAuthentication(key, token string) HeaderOption {
+	return func(header map[string]string) {
+		header[key] = token
+	}
+}
 
 // RoundTripperWithHeaders is a round tripper that adds headers to the request.
 type RoundTripperWithHeaders struct {
@@ -14,7 +36,13 @@ type RoundTripperWithHeaders struct {
 }
 
 // NewRoundTripperWithHeaders creates a new RoundTripperWithHeaders.
-func NewRoundTripperWithHeaders(headers map[string]string, next http.RoundTripper) *RoundTripperWithHeaders {
+func NewRoundTripperWithHeaders(next http.RoundTripper, headerOptions ...HeaderOption) *RoundTripperWithHeaders {
+	headers := make(map[string]string)
+
+	for _, option := range headerOptions {
+		option(headers)
+	}
+
 	return &RoundTripperWithHeaders{
 		headers: headers,
 		next:    next,
