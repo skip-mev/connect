2675bbc89972417bc531d312f59f4611e9544069
aljo242 <alex@skip.money>
2024-07-09 10:25:36 -0400

    gen

--- providers/apis/defi/osmosis/client.go
+++ providers/apis/defi/osmosis/client.go
@@ -1,15 +1,33 @@
 package osmosis
 
 import (
-	gammtypes "github.com/osmosis-labs/osmosis/v25/x/gamm/types"
+	"context"
+
+	"github.com/osmosis-labs/osmosis/v25/x/poolmanager/client/queryproto"
 
 	"github.com/skip-mev/slinky/oracle/config"
 	"github.com/skip-mev/slinky/providers/base/api/metrics"
 )
 
-type GRPCCLient struct {
+var _ GRPCCLient = &GRPCCLientImpl{}
+
+// GRPCClient is the expected interface for an osmosis grpc client.
+//
+//go:generate mockery --name GRPCCLient --output ./mocks/ --case underscore
+type GRPCCLient interface {
+	SpotPrice(grpcCtx context.Context,
+		req *queryproto.SpotPriceRequest,
+	) (*queryproto.SpotPriceResponse, error)
+}
+
+type GRPCCLientImpl struct {
 	api        config.APIConfig
 	apiMetrics metrics.APIMetrics
 
-	gammClient gammtypes.QueryClient
+	pmClient queryproto.QueryClient
+}
+
+func (c *GRPCCLientImpl) SpotPrice(grpcCtx context.Context, req *queryproto.SpotPriceRequest) (*queryproto.SpotPriceResponse, error) {
+	// TODO implement me
+	panic("implement me")
 }
