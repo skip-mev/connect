82a89a4aecc314293b1a999322644b48f1cdea3c
David Terpay <david.terpay@gmail.com>
2024-07-10 10:29:09 -0400

    update interval

--- providers/apis/bitstamp/utils.go
+++ providers/apis/bitstamp/utils.go
@@ -25,7 +25,7 @@ var DefaultAPIConfig = config.APIConfig{
 	Atomic:           true,
 	Enabled:          true,
 	Timeout:          3000 * time.Millisecond,
-	Interval:         2000 * time.Millisecond,
+	Interval:         3000 * time.Millisecond,
 	ReconnectTimeout: 2000 * time.Millisecond,
 	MaxQueries:       1,
 	Endpoints:        []config.Endpoint{{URL: URL}},
