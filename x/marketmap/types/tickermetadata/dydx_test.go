5c8ddc09c07aae3dca39001d4148ed1cad75f2c2
David Terpay <david.terpay@gmail.com>
2024-07-23 11:54:15 -0400

    code cov

--- x/marketmap/types/tickermetadata/dydx_test.go
+++ x/marketmap/types/tickermetadata/dydx_test.go
@@ -7,7 +7,7 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func Test_Unmarshal(t *testing.T) {
+func Test_UnmarshalDyDx(t *testing.T) {
 	t.Run("can marshal and unmarshal the same struct and values", func(t *testing.T) {
 		elem := tickermetadata.NewDyDx(
 			100,
@@ -50,5 +50,26 @@ func Test_Unmarshal(t *testing.T) {
 				tickermetadata.NewAggregatorID("cmc", "id"),
 			},
 		), elem)
+	})
+}
+
+func Test_UnmarshalAggregatorID(t *testing.T) {
+	t.Run("can marshal and unmarshal the same struct and values", func(t *testing.T) {
+		elem := tickermetadata.NewAggregatorID("coingecko", "id")
+
+		bz, err := tickermetadata.MarshalAggregatorID(elem)
+		require.NoError(t, err)
+
+		elem2, err := tickermetadata.AggregatorIDFromJSONBytes(bz)
+		require.NoError(t, err)
+		require.Equal(t, elem, elem2)
+	})
+
+	t.Run("can unmarshal a JSON string into a struct", func(t *testing.T) {
+		elemJSON := `{"venue":"coingecko","ID":"id"}`
+		elem, err := tickermetadata.AggregatorIDFromJSONString(elemJSON)
+		require.NoError(t, err)
+
+		require.Equal(t, tickermetadata.NewAggregatorID("coingecko", "id"), elem)
 	})
 }
