b1bb8381918175ce7cd71867854ada6ae551cfd7
Alex Johnson <alex@skip.money>
2024-07-25 14:26:54 -0400

    feat: osmosis provider (#607)
    Co-authored-by: David Terpay <35130517+davidterpay@users.noreply.github.com>

---
+++ providers/apis/defi/osmosis/types_test.go
@@ -0,0 +1,49 @@
+package osmosis_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+
+	"github.com/skip-mev/slinky/providers/apis/defi/osmosis"
+)
+
+func TestCreateURL(t *testing.T) {
+	type args struct {
+		baseURL    string
+		poolID     uint64
+		baseAsset  string
+		quoteAsset string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    string
+		wantErr bool
+	}{
+		{
+			name: "basic",
+			args: args{
+				baseURL:    "http://localhost",
+				poolID:     1,
+				baseAsset:  "base",
+				quoteAsset: "quote",
+			},
+			want: "http://localhost/osmosis/poolmanager/v2/pools/1/prices?base_asset_denom=base&quote_asset_denom" +
+				"=quote",
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := osmosis.CreateURL(tt.args.baseURL, tt.args.poolID, tt.args.baseAsset, tt.args.quoteAsset)
+			if tt.wantErr {
+				require.Error(t, err)
+				return
+			}
+
+			require.NoError(t, err)
+			require.Equal(t, tt.want, got)
+		})
+	}
+}
