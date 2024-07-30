f193bc4fff611b2132d16562c71a02df834daf17
Tyler <48813565+technicallyty@users.noreply.github.com>
2024-07-30 11:58:01 -0700

    add test for polymarket provider, reconfigure test

--- cmd/constants/marketmaps/markets_test.go
+++ cmd/constants/marketmaps/markets_test.go
@@ -1,43 +1,24 @@
 package marketmaps_test
 
 import (
-	"encoding/json"
 	"testing"
 
 	"github.com/stretchr/testify/require"
 
 	"github.com/skip-mev/slinky/cmd/constants/marketmaps"
-	mmtypes "github.com/skip-mev/slinky/x/marketmap/types"
+	marketmaptypes "github.com/skip-mev/slinky/x/marketmap/types"
 )
 
 func TestMarkets(t *testing.T) {
-	// Unmarshal the RaydiumMarketMapJSON into RaydiumMarketMap.
-	var mm mmtypes.MarketMap
-	require.NoError(t, json.Unmarshal([]byte(marketmaps.RaydiumMarketMapJSON), &mm))
-	require.NoError(t, mm.ValidateBasic())
-
-	// Unmarshal the CoreMarketMapJSON into CoreMarketMap.
-	var mm2 mmtypes.MarketMap
-	require.NoError(t, json.Unmarshal([]byte(marketmaps.CoreMarketMapJSON), &mm2))
-	require.NoError(t, mm2.ValidateBasic())
-
-	// Unmarshal the UniswapV3BaseMarketMapJSON into UniswapV3BaseMarketMap.
-	var mm3 mmtypes.MarketMap
-	require.NoError(t, json.Unmarshal([]byte(marketmaps.UniswapV3BaseMarketMapJSON), &mm3))
-	require.NoError(t, marketmaps.UniswapV3BaseMarketMap.ValidateBasic())
-
-	// Unmarshal the CoinGeckoMarketMapJSON into CoinGeckoMarketMap.
-	var mm4 mmtypes.MarketMap
-	require.NoError(t, json.Unmarshal([]byte(marketmaps.CoinGeckoMarketMapJSON), &mm4))
-	require.NoError(t, marketmaps.CoinGeckoMarketMap.ValidateBasic())
-
-	// Unmarshal the CoinMarketCapMarketMapJSON into CoinMarketCapMarketMap.
-	var mm5 mmtypes.MarketMap
-	require.NoError(t, json.Unmarshal([]byte(marketmaps.CoinMarketCapMarketMapJSON), &mm5))
-	require.NoError(t, marketmaps.CoinMarketCapMarketMap.ValidateBasic())
-
-	// Unmarshal the OsmosisMarketMapJSON into OsmosisMarketMap.
-	var mm6 mmtypes.MarketMap
-	require.NoError(t, json.Unmarshal([]byte(marketmaps.OsmosisMarketMapJSON), &mm6))
-	require.NoError(t, marketmaps.OsmosisMarketMap.ValidateBasic())
+	markets := []map[string]marketmaptypes.Market{
+		marketmaps.RaydiumMarketMap.Markets,
+		marketmaps.CoreMarketMap.Markets,
+		marketmaps.UniswapV3BaseMarketMap.Markets,
+		marketmaps.CoinGeckoMarketMap.Markets,
+		marketmaps.OsmosisMarketMap.Markets,
+		marketmaps.PolymarketMarketMap.Markets,
+	}
+	for _, m := range markets {
+		require.NotEmpty(t, m)
+	}
 }
