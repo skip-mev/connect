3cc60300c7581403aa447cc262bfc685a9eab4cb
aljo242 <alex@skip.money>
2024-09-09 15:45:22 -0400

    add validate

--- service/validation/validation.go
+++ service/validation/validation.go
@@ -11,10 +11,10 @@ import (
 )
 
 const (
-	DefaultNumChecks             = 1000
-	DefaultRequiredPriceLiveness = 99.0
-	DefaultValidationPeriod      = 10 * time.Minute
-	DefaultBurnInPeriod          = 60 * time.Second
+	DefaultNumChecks                    = 1000
+	DefaultRequiredPriceLivenessPercent = 99.0
+	DefaultValidationPeriod             = 10 * time.Minute
+	DefaultBurnInPeriod                 = 60 * time.Second
 )
 
 type Validator struct {
@@ -39,26 +39,49 @@ func NewValidator(logger *zap.Logger, metrics oraclemetrics.Metrics, cfg Config)
 
 // Config includes information for configuring a validation service instance.
 type Config struct {
-	BurnInPeriod          time.Duration
-	ValidationPeriod      time.Duration
-	NumChecks             int
-	RequiredPriceLiveness float64
+	// BurnInPeriod is the amount of time to let the sidecar run before checking validation.
+	// This ensures that any markets or prices that take a few iterations to land will be populated,
+	// eliminating false positives.
+	BurnInPeriod time.Duration
+	// ValidationPeriod is the amount of time the validation service will check that prices are landing correctly.
+	ValidationPeriod time.Duration
+	// NumChecks is the number of times the validation service will check validity over the ValidationPeriod.
+	NumChecks int
+	// RequiredPriceLivenessPercent is the percentage of liveness each price must demonstrate to be considered "valid".
+	RequiredPriceLivenessPercent float64
 }
 
 // DefaultConfig returns a default validation config.
 func DefaultConfig() Config {
 	return Config{
-		BurnInPeriod:          DefaultBurnInPeriod,
-		NumChecks:             DefaultNumChecks,
-		RequiredPriceLiveness: DefaultRequiredPriceLiveness,
-		ValidationPeriod:      DefaultValidationPeriod,
+		BurnInPeriod:                 DefaultBurnInPeriod,
+		NumChecks:                    DefaultNumChecks,
+		RequiredPriceLivenessPercent: DefaultRequiredPriceLivenessPercent,
+		ValidationPeriod:             DefaultValidationPeriod,
 	}
+}
+
+// Validate checks the validity of fields in the Config.
+func (c *Config) Validate() error {
+	if c.NumChecks <= 0 {
+		return fmt.Errorf("num checks must be greater than zero")
+	}
+
+	if c.RequiredPriceLivenessPercent <= 0 {
+		return fmt.Errorf("required price liveness percent must be greater than zero")
+	}
+
+	return nil
 }
 
 type LivenessResults map[string]float64
 
 // Run runs the validation service, checking for missing prices and accumulating liveness data.
 func (v *Validator) Run(ctx context.Context) (LivenessResults, error) {
+	if err := v.cfg.Validate(); err != nil {
+		return nil, fmt.Errorf("config validation error: %w", err)
+	}
+
 	v.logger.Info("running in validation mode", zap.Duration("validation period (s)",
 		v.cfg.ValidationPeriod))
 
@@ -116,9 +139,9 @@ func (v *Validator) Run(ctx context.Context) (LivenessResults, error) {
 		livenessRate := float64(v.cfg.NumChecks-ticksMissing) / float64(v.cfg.NumChecks)
 		resultsMap[pairID] = livenessRate
 
-		if livenessRate < v.cfg.RequiredPriceLiveness {
+		if livenessRate < v.cfg.RequiredPriceLivenessPercent {
 			v.logger.Error("liveness for price is below required rate", zap.String("pairID", pairID),
-				zap.Float64("liveness_rate", livenessRate), zap.Float64("required", v.cfg.RequiredPriceLiveness))
+				zap.Float64("liveness_rate", livenessRate), zap.Float64("required", v.cfg.RequiredPriceLivenessPercent))
 			invalidTickers = append(invalidTickers, pairID)
 		}
 	}
