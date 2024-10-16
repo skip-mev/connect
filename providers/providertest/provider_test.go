9d316d9d90e9e7b48ad26e76cf395bbb30883b8a
aljo242 <alex@skip.money>
2024-10-16 00:00:17 -0400

    add file for demo

--- providers/providertest/provider_test.go
+++ providers/providertest/provider_test.go
@@ -29,25 +29,3 @@ func TestUniswapMarkets(t *testing.T) {
 		}
 	}
 }
-
-func TestUniswapMarketsIsolated(t *testing.T) {
-	mm, err := mmtypes.ReadMarketMapFromFile("./output.json")
-	require.NoError(t, err)
-
-	providerMM := providertest.FilterMarketMapToProviders(mm)
-	for provider, mm := range providerMM {
-		if provider == "uniswapv3_api-ethereum" {
-			ctx := context.Background()
-			o, err := providertest.NewTestingOracle(ctx, provider)
-			require.NoError(t, err)
-
-			for name, market := range mm.Markets {
-				res, err := o.RunMarket(ctx, market, providertest.DefaultProviderTestConfig())
-				require.NoError(t, err)
-
-				t.Log(name, res)
-			}
-
-		}
-	}
-}
